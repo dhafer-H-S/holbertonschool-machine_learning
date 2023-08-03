a2720f3ffe6d1db4e36286bad8583f347d8f3dcf
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-03 15:20:21 +0100

    task-16-

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -5,15 +5,33 @@
 """ class of a DeepNeuralNetwork """
 import numpy as np
 
-
 class DeepNeuralNetwork:
     def __init__(self, nx, layers):
+        """
+        Initializes a deep neural network performing binary classification.
+
+        Parameters:
+        nx (int): Number of input features.
+        layers (list): List representing the number of nodes in each layer of the network.
+
+        Raises:
+        TypeError: If nx is not an integer or layers is not a list.
+        ValueError: If nx is less than 1.
+        TypeError: If layers is an empty list or contains non-positive integers.
+
+        Attributes:
+        L (int): The number of layers in the neural network.
+        cache (dict): A dictionary to hold all intermediary values of the network.
+        weights (dict): A dictionary to hold all weights and biases of the network.
+        """
+        # Check if nx is an integer and positive
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         
-        if not isinstance(layers, list) or len(layers) == 0 or not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        # Check if layers is a list and contains positive integers
+        if not isinstance(layers, list) or len(layers) == 0 or not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers) # number of layers in the neural network
