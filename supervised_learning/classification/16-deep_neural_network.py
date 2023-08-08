fe191ea7b85874cf8a6b6d8ab13470aad06fdebb
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-08 12:22:37 +0100

    task-16-

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -13,7 +13,6 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
         # check if the elements in the layers are integers or not 
@@ -28,16 +27,10 @@ class DeepNeuralNetwork:
         # loop to iterates through the range of numbers of layers 
         for l in range(1, self.L + 1):
 
-            # initialise the weight if we are working on the first layer we 
-            # gonna use He et al as intialize methode to generate weights with
-            # a shape based on layers(l) and nx ( inputes features ) and then 
-            # scale them usnig square root 2 / nx
+
             he_et_al = np.sqrt(2 / layer_size)
-            #for layers other than the first one it genrates random weigths
-            # with a shape layers(l) - layers(l - 1) and scale them using
-            # the square root of 2 / layers(l - 1)   
+
             self.weights['W' + str(l)] = np.random.randn(layers[l - 1], layer_size) * he_et_al
-            # initialize the biases for the current layer with zeros and store
-            # them in the weights
+
             self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
             layer_size = layers[l - 1]
