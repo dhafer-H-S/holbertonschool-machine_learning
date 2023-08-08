74b38e29fe9f4cb4b519dd422cdf5c529e6cea80
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-08 12:59:50 +0100

    task-16-

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -16,10 +16,18 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        # check if the elements in the layers are integers or not
-        # and check if theyeare greater then 0 or not
         if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
             raise TypeError("layers must be a list of positive integers")
+        # if not isinstance(nx, int):
+        #     raise TypeError("nx must be an integer")
+        # if nx < 1:
+        #     raise ValueError("nx must be a positive integer")
+        # if not isinstance(layers, list) or not layers:
+        #     raise TypeError("layers must be a list of positive integers")
+        # # check if the elements in the layers are integers or not
+        # # and check if theye are greater then 0 or not
+        # if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
+        #     raise TypeError("layers must be a list of positive integers")
         # set public instance attributes
         self.L = len(layers)
         self.cache = {}
@@ -38,11 +46,9 @@ class DeepNeuralNetwork:
             them in the weights
         """
         for l in range(1, self.L + 1):
-
             layer_size = nx
             he_et_al = np.sqrt(2 / layer_size)
             self.weights["W" + str(l)] = np.random.randn(layers[l - 1],
                                                          layer_size) * he_et_al
-
             self.weights["b" + str(l)] = np.zeros((layers[l - 1], 1))
             layer_size = layers[l - 1]
