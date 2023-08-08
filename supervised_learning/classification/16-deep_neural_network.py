04730d2d4bf34eabc1ce98b5935ca895a8dc9cc5
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-08 11:59:54 +0100

    task-16-

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,43 +1,30 @@
 #!/usr/bin/env python3
 
-""" DeepNeuralNetwork """
-
-""" class of a DeepNeuralNetwork """
+"""deep neural network"""
 import numpy as np
 
+
 class DeepNeuralNetwork:
-    # deff methode to initializa the deep neural network
+    """deep neural network"""
+
     def __init__(self, nx, layers):
-        # check conditions 
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        # set public instance attributes
+        if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
+            raise TypeError("layers must be a list of positive integers")
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        # loop to iterates through the range of numbers of layers 
-        for l in range(self.L):
-            # check if the elements in the layers are integers or not 
-            # and check if theyeare greater then 0 or not 
-            if not isinstance(layers[l], int) or layers[l] <= 0:
-                raise TypeError("layers must be a list of positive integers")
-            # initialise the weight if we are working on the first layer we 
-            # gonna use He et al as intialize methode to generate weights with
-            # a shape based on layers(l) and nx ( inputes features ) and then 
-            # scale them usnig square root 2 / nx
-            
-            if l == 0:
-                self.weights['W' + str(l + 1)] = np.random.randn(layers[l], nx) * np.sqrt(2 / nx)
-            else:
-                #for layers other than the first one it genrates random weigths
-                # with a shape layers(l) - layers(l - 1) and scale them using
-                # the square root of 2 / layers(l - 1)
-                self.weights['W' + str(l + 1)] = np.random.randn(layers[l], layers[l - 1]) * np.sqrt(2 / layers[l - 1])
-            # initialize the biases for the current layer with zeros and store
-            # them in the weights
-            self.weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
+        layer_size = nx
+
+        for i in range(1, self.L + 1):
+            he_et_al = np.sqrt(2 / layer_size)
+            self.weights["W" + str(i)] = np.random.randn(layers[i - 1],
+                                                         layer_size) * he_et_al
+            self.weights["b" + str(i)] = np.zeros((layers[i - 1], 1))
+            layer_size = layers[i - 1]
