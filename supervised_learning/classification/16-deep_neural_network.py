e24368a7be52d3492825a3f2900af4398b5ea8e0
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-08 12:47:44 +0100

    task-16-

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -8,22 +8,22 @@ import numpy as np
 class DeepNeuralNetwork:
     # deff methode to initializa the deep neural network
     def __init__(self, nx, layers):
-        # check conditions 
+        # check conditions
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        # check if the elements in the layers are integers or not 
-        # and check if theyeare greater then 0 or not 
+        # check if the elements in the layers are integers or not
+        # and check if theyeare greater then 0 or not
         if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
             raise TypeError("layers must be a list of positive integers")
         # set public instance attributes
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        
+
         # loop to iterates through the range of numbers of layers 
         for l in range(1, self.L + 1):
             # initialise the weight if we are working on the first layer we 
