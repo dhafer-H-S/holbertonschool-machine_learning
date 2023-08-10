818b3ae012bd49c0252827cf0fbf34aeffe71425
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-10 21:55:05 +0100

    task-18-

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -52,8 +52,7 @@ class DeepNeuralNetwork:
             self.__weights["W" + str(l)] = np.random.randn(
                 layers[l - 1], layer_size) * he_et_al
             self.__weights["b" + str(l)] = np.zeros((layers[l - 1], 1))
-            layer_size = layers[l - 1]
-            
+            layer_size = layers[l - 1]  
 
     """ getters for private attributes """
     @property
@@ -67,7 +66,7 @@ class DeepNeuralNetwork:
     @property
     def weights(self):
         return self.__weights
-    
+
     """ def methode for froward propagation for the neural netwok"""
     def forward_prop(self, X):
         self.__cache['A0'] = X
