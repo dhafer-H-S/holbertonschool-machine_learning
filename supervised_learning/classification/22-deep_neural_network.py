e85df801f010606a0b81554f9d744336d2834e33
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-16 23:06:47 +0100

    task-22-

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -129,11 +129,11 @@ class DeepNeuralNetwork:
         m = Y.shape[1]
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
-        if not iterations >= 0:
+        if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-        if not alpha >= 0:
+        if alpha <= 0:
             raise ValueError("alpha must be positive")
         for l in range(iterations):
             A, cache = self.forward_prop(X)
