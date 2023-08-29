0652e8eaaebcd18a013b548a59e1941c681b739b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-29 10:35:18 +0200

    task-0-

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -9,8 +9,8 @@ def create_placeholders(nx, classes):
     """nx the number of feature colims in our data"""
     """number of classes in our classifier"""
     """ x place holder for the inpute data"""
-    """ y place holder for the one hot labels for the inpute data"""
     x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    """ y place holder for the one hot labels for the inpute data"""
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
-
+    """ return place holders for the data"""
     return x, y
