471fee645345f98e32f4c30aa9637e8768c48f1f
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-08-30 15:41:56 +0200

    task-7-

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 import tensorflow.compat.v1 as tf
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-forward_prop = __import__('2-forward_prop').forward_prop
 
 def evaluate(X, Y, save_path):
     """Evaluate the output of a neural network.
@@ -17,19 +13,19 @@ def evaluate(X, Y, save_path):
     Returns:
         The network's prediction, accuracy, and loss.
     """
-    tf.reset_default_graph()
-
-    x, y = create_placeholders(X.shape[1], Y.shape[1])
-    y_pred = forward_prop(x, [100], [tf.nn.relu])
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-
-    saver = tf.train.Saver()
-
     with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
-        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
-        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
-        loss_value = sess.run(loss, feed_dict={x: X, y: Y})
 
-    return prediction, acc, loss_value
+        graph = tf.get_default_graph()
+        x = graph.get_tensor_by_name('x:0')
+        y = graph.get_tensor_by_name('y:0')
+        y_pred = graph.get_tensor_by_name('y_pred:0')
+        accuracy = graph.get_collection('accuracy')[0]
+        loss = graph.get_collection('loss')[0]
+
+        feed_dict = {x: X, y: Y}
+        y_pred_val, accuracy_val, loss_val = sess.run(
+            [y_pred, accuracy, loss], feed_dict=feed_dict)
+
+    return y_pred_val, accuracy_val, loss_val
