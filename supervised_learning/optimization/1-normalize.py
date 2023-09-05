690d9a27ef247fa85ebc71d91c16e461b6075050
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-05 18:25:26 +0200

    task-1-

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -4,7 +4,15 @@
 
 import numpy as np
 
-
+""" normalize data"""
 def normalize(X, m, s):
+    """
+    x is the data with sahpe (d, nx)
+    d is  the number of data point
+    nx is th number of features
+    """
+    """ m is the mean of all features of x"""
+    """ s contains the standar deviation of all features of x"""
+    """ the function to calculate the normilasied data"""
     normalized_X = (X - m) / s
     return normalized_X
