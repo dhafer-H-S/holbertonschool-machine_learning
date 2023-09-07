bbbf0e2d7047824190178473499f61cbb0bd9823
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-07 05:37:51 +0200

    task-3-

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -23,7 +23,7 @@ def train_mini_batch(
         train_op = tf.get_collection('train_op')[0]
 
         for i in range(epochs):
-            print('After {} epochs: '.format(i))
+            print('After {} epochs:'.format(i))
             print('After {} epochs:'.format(i))
             train_cost, train_accuracy = sess.run(
                 (loss, accuracy), feed_dict={
