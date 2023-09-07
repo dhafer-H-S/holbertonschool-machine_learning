9d1672045ee7ae4467c681ba0d7f6dab9e8a65c3
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-07 05:11:13 +0200

    task-3-

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 
-"""
-function trains a loaded neural network model using mini batch gradient descent
-"""
+
 import tensorflow.compat.v1 as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 
+"""
+function trains a loaded neural network model using mini batch gradient descent
+"""
 def train_mini_batch(
         X_train,
         Y_train,
