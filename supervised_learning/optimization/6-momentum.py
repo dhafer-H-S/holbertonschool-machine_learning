611ce7733529a51a32d8a20733c32dac0983abb8
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-08 19:24:31 +0200

    task-6-

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """
-function that createsthe training operation for neural network 
-in tenser flow using gradient descent with momentum 
+function that createsthe training operation for neural network
+in tenser flow using gradient descent with momentum
 optimization algorithm
 """
+
+
 import tensorflow.compat.v1 as tf
 
 def create_momentum_op(loss, alpha, beta1):
