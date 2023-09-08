b69bdceef4dd1b0fa22000284220966b93ee6913
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-08 15:54:42 +0200

    task-5-

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" update the gradient descent with momentum optimization algorithms """
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    alpha: is the learning rate
+    beta1: is the momentum weight
+    var: is a numpy.ndarray containing the variable to be updated
+    grad: is a numpy.ndarray containing the gradient of var
+    v: is the previous first moment of var
+    """
+    v = beta1 * v + alpha * grad
+    var -= v
+    return var, v
