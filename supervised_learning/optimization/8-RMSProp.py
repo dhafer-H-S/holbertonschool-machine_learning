30ce291ec5a0538d646b1644e3e7fb15be09bc3f
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-11 00:21:40 +0200

    task-8-

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """ RMSProp optimzation with tenserflow function """
+
+
 import tensorflow.compat.v1 as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    create the training operation for a neural network in tenserflow using RMSProp optimization algorithm
+    """
+    """ alpha learning rate """
+    """ beta2 the RMSProp weight """
+    """ epilson is a small  number to avid divisin by zero """
+    """ loss of the model network """
+
     optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
     return optimizer.minimze(loss)
