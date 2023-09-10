b87717e9a143ec31eab8c39f6cb2a15a982f56f2
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-11 00:15:35 +0200

    task-8-

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -4,5 +4,5 @@ import tensorflow.compat.v1 as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    optimizer = tf.compat.v1.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
     return optimizer.minimze(loss)
