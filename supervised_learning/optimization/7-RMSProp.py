ba59dc91ed346b331dbcead927b80d9358bb3b18
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-10 21:56:20 +0200

    task-7-

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,20 @@
+#!/usr/biin/env python3
+""" RMS_prop optimizer that updates the parameters """
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """ alpha learning rate """
+    """ beta2 the RMSProp weight """
+    """ epilson is a small  number to avid divisin by zero """
+    """ var containing the variable to be updated """
+    """ grad containing the gradient of var """
+    """ S is the previous second moment of var """
+
+    """ computes the squared gradient """
+    gred_squared = grad ** 2
+    """ update the seconde moment (S) using exponential moving average """
+    """ s = exp(gred_squared) """
+    s_updated = beta2 * s + (1 - beta2) * gred_squared
+    """ update the variable using the rms prop updates rule """
+    var = var - alpha / (np.sqrt( s + epsilon) * grad)
