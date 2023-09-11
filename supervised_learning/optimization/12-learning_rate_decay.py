2591af5a15bccfc186cedd2397b16465e3959996
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-11 06:03:27 +0200

    task-12-

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -8,6 +8,9 @@ import tensorflow.compat.v1 as tf
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ function to calculate decay operation using tensorflow predefined function """
 
-    decay = tf.compat.v1.train.inverse_time_decay(
-        alpha, global_step, decay_step, decay_rate)
+    decay = tf.train.inverse_time_decay(
+        learning_rate=alpha,
+        global_step=global_step,
+        decay_step=decay_step,
+        decay_rate=decay_rate)
     return decay
