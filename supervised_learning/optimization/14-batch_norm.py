dcb7357bf41f635714b44ba33da99f9883c2d60c
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-12 13:00:11 +0200

    task-14-

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+a function stabilize and accelerate the training process in
+neural network using batch normalization predefined tensorflow function
+"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+    Create a batch normalization layer for a neural network in TensorFlow.
+
+    prev: Activated output of the previous layer.
+    n: Number of nodes in the layer to be created.
+    activation: Activation function to be used on the output of the layer.
+    """
+    """Create a Dense layer with VarianceScaling initializer"""
+    dense_layer = tf.keras.layers.Dense(
+        n, kernel_initializer=tf.keras.initializers.VarianceScaling(
+            mode='fan_avg'))
+
+    """Apply the Dense layer to the previous layer"""
+    dense_output = dense_layer(prev)
+
+    """Create a BatchNormalization layer with trainable parameters gamma and beta"""
+    batch_norm_layer = tf.keras.layers.BatchNormalization(epsilon=1e-8)
+
+    """Apply BatchNormalization to the output of the Dense layer"""
+    bn_output = batch_norm_layer(dense_output)
+
+    """Apply the activation function to the normalized output"""
+    activated_output = activation(bn_output)
+
+    return activated_output
