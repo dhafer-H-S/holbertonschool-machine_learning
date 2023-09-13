8df1363cece96da969b839bdd6240df84c78fd20
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-14 01:57:36 +0200

    task-14-

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -14,22 +14,30 @@ def create_batch_norm_layer(prev, n, activation):
     n: Number of nodes in the layer to be created.
     activation: Activation function to be used on the output of the layer.
     """
-    dense_layer = tf.layers.Dense(
-        units=n,
-        kernel_initializer=tf.contrib.layers.variance_scaling_initializer(
-            mode="FAN_AVG"),
-        use_bias=False)
+    def create_batch_norm_layer(prev, n, activation):
+        init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+        """
+        init is an initializer used to inisialize the weights 
+        f the dense layer
+        """
+        layer = tf.keras.layers.Dense(n, kernel_initializer=init)
+        """
+        desne layer created with 'n' unites and initialized with the
+        previously defined initializer
+        """
+        z = layer(prev)
+        gamma = tf.Variable(1., trainable=True)
+        beta = tf.Variable(0., trainable=True)
+        """ gamma and beta are trainable variables will be used for
+        scaling and shifting during batch normalization """
+        mean = tf.math.reduce_mean(z, axis=0)
+        var = tf.math.reduce_variance(z, axis=0)
+        epsilon = 1e-8
+        normalized = tf.nn.batch_normalization(z, mean, var, beta, gamma, epsilon)
+        """
+        batch normalization is applied to the pre-activation 'z'
+        it uses the calculated mean, variance, beta, gamma and epsilon
+        for normalization
+        """
+        return activation(normalized)
 
-    dense_output = dense_layer(prev)
-
-    batch_norm_layer = tf.layers.BatchNormalization(
-        epsilon=1e-8,
-        trainable=True,
-        gamma_initializer=tf.ones_initializer(),
-        beta_initializer=tf.zeros_initializer())
-
-    bn_output = batch_norm_layer(dense_output)
-
-    activated_output = activation(bn_output)
-
-    return activated_output
