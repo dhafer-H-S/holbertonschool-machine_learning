c6cbccc1666eca79aae1b4a2d54dfcf3ed74e06c
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-14 01:58:14 +0200

    task-14-

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -17,7 +17,7 @@ def create_batch_norm_layer(prev, n, activation):
     def create_batch_norm_layer(prev, n, activation):
         init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
         """
-        init is an initializer used to inisialize the weights 
+        init is an initializer used to inisialize the weights
         f the dense layer
         """
         layer = tf.keras.layers.Dense(n, kernel_initializer=init)
@@ -33,11 +33,11 @@ def create_batch_norm_layer(prev, n, activation):
         mean = tf.math.reduce_mean(z, axis=0)
         var = tf.math.reduce_variance(z, axis=0)
         epsilon = 1e-8
-        normalized = tf.nn.batch_normalization(z, mean, var, beta, gamma, epsilon)
+        normalized = tf.nn.batch_normalization(
+            z, mean, var, beta, gamma, epsilon)
         """
         batch normalization is applied to the pre-activation 'z'
         it uses the calculated mean, variance, beta, gamma and epsilon
         for normalization
         """
         return activation(normalized)
-
