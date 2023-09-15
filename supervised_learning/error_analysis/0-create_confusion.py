69d3b5e4a2da3356e6a785f34b21376c37c90d54
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-15 04:38:52 +0200

    task-0-

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -9,16 +9,15 @@ def create_confusion_matrix(labels, logits):
     logits are the predicted labels
     """
     classes = labels.shape[1]
-    
+
     # Initialize the confusion matrix with zeros
     confusion = np.zeros(classes, classes)
-    
+
     # Iterate through each data point
     for i in range(labels.shape[0]):
         # Find the index of the correct label (truth) and predicted label
         true_label = np.argmax(labels[i])
         predicted_label = np.argmax(logits[i])
-        
         # Increment the corresponding cell in the confusion matrix
         confusion[true_label, predicted_label] += 1
     
