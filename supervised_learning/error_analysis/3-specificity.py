1e5263828853a1602b3a0fc1f930cf0d7da920b1
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-16 08:12:33 +0200

    task-3-

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -6,22 +6,16 @@ import numpy as np
 
 
 def specificity(confusion):
+    """ calcule specificity """
     classes = confusion.shape[0]
     specificties = np.zeros(classes)
     for i in range(classes):
         True_Positive = confusion[i, i]
-
         total_predicted_values = np.sum(confusion[:, i])
-
         total_correct_value = np.sum(confusion[i, :])
-
         false_positive = total_predicted_values - True_Positive
-
         false_negative = total_correct_value - True_Positive
-
         True_negative = np.sum(confusion) - (True_Positive + false_positive + false_negative)
-
         specificity = True_negative / (True_negative + false_positive)
-
         specificties[i] = specificity
     return specificties
