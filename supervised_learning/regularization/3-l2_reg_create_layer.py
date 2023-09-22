00881889d7ab17499e6dd28f8938f2f701b499ce
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-22 16:47:30 +0200

    task-3-

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" a tensorflow layer that includes L2 regularization """
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    prev: is a tensor containgn the output of the previous layer
+    n: is the number of nodes the new layer should contain
+    activation: is the activation function that should be used on the layer
+    lambtha: the L2 regularization parameter
+    """
+    layer = tf.layers.dense(
+        units=n,
+        activation=activation,
+        kernel_reglarization=lambtha
+    )(prev)
+    return layer
