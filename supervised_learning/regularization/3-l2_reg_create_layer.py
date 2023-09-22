d14495fba940b0f0821cb551988dbd16b6d6019b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-22 18:14:03 +0200

    task-3-

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-"""L2 Regulazation cost layer in tensorflow."""
+""" a tensorflow layer that includes L2 regularization """
 import tensorflow.compat.v1 as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Create layer with L2 Regularization."""
-    l2 = tf.keras.regularizers.L2(lambtha)
-    weight = tf.keras.initializers.VarianceScaling(scale=2.0,
-                                                   mode="fan_avg")
+    """
+    prev: is a tensor containgn the output of the previous layer
+    n: is the number of nodes the new layer should contain
+    activation: is the activation function that should be used on the layer
+    lambtha: the L2 regularization parameter
+    """
+    L2_regularization = tf.keras.regularizers.L2(lambtha)
+    """a regularier that applies a L2 regularization penalty"""
+    layer_weight = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    """initializer that adapt tits scale tot the shape og it's inpute tensors"""
+
+
     layer = tf.layers.Dense(n, activation=activation,
-                            kernel_initializer=weight,
-                            kernel_regularizer=l2)
+                            kernel_initializer=layer_weight,
+                            kernel_reglarization=L2_regularization)
     return layer(prev)
