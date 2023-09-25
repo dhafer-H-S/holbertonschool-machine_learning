2d23b7ae1e090092d10459db4fd76201f6d7b47b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-25 10:46:58 +0200

    task-4-

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -2,38 +2,40 @@
 """a function that conducts forword propagation using dropout"""
 import numpy as np
 
-
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
-    X : containg the inpute data for the neural network
-    X shape(nx, m)
-    nx number of input feature
-    m is the number of data points
+    Conduct forward propagation using dropout for a neural network.
 
-    weights :  is a dictionary of the weights and biases of the neural network
-    L: number of layers
-    keep_prob : propability that node will kept
+    Args:
+        X: Input data as a numpy array of shape (nx, m).
+           - nx: Number of input features.
+           - m: Number of data points.
+        weights: Dictionary of weights and biases for the neural network.
+        L: Number of layers in the network.
+        keep_prob: Probability that a node will be kept during dropout.
+
+    Returns:
+        A dictionary containing the outputs of each layer and dropout masks.
     """
     output_layer = {}
-    dropout_mask = {}
+    dropouts = {}
+
     for i in range(1, L + 1):
         if i < L:
             Z = np.dot(weights['W' + str(i)], X) + weights['b' + str(i)]
-            activation = np.tanh(Z)
-            """activation with tanh activation function"""
-            dropout_mask = (
-                np.random.rand(
-                    activation.shape[0],
-                    activation.shape[1]) < keep_prob)
-            """dropout"""
-            activation *= dropout_mask / keep_prob
-            output_layer['activation' + str(i)] = activation
-            dropout_mask['D' + str(i)] = dropout_mask
+            A = np.tanh(Z)  # Activation with tanh activation function
 
-            X = activation
+            dropout_mask = (np.random.rand(A.shape[0], A.shape[1]) < keep_prob)
+            A *= dropout_mask / keep_prob
+
+            output_layer['activation' + str(i)] = A
+            dropouts['D' + str(i)] = dropout_mask
+
+            X = A
         else:
             Z = np.dot(weights['W' + str(i)], X) + weights['b' + str(i)]
             A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
 
             output_layer['activation' + str(i)] = A
-    return {'output_layer': output_layer, 'dropout_mask': dropout_mask}
+
+    return {'output_layer': output_layer, 'dropout_masks': dropouts}
