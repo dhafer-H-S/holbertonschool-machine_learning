4622f6e6549768c8d0e280b5d35784b063a6d1a6
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-25 15:07:30 +0200

    task-5-

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -12,31 +12,19 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     keep_prob the propability that node will be kept
     L is the number of layers that a node will be kept
     """
-    def tanh(Z):
-        """activation function using tanh"""
-        return ((np.exp(Z) - np.exp(Z)) / (np.exp(Z) + np.exp(Z)))
-    def softmax(Z):
-        """activation function using softmax"""
-        exp = np.exp(Z)
-        return exp / exp.sum(axis=0, keepdims=True)
-    def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-        m= Y.shape[1]
-        """m number of daat point"""
-        for i in range(L, 0, -1):
-            if i == L:
-                Z = ((np.dot(weights['W' + str(i)], weights['A' + str(i)]) + weights['b' + str(i)]))
-                dZ = cache['A' + str(i)] - Y
-                """calculate the gradient of the last layer using softmax """
-        else:
-            """Calculate the gradient of hidden layers using tanh activation"""            
-            dA = np.dot(weights['W' + str(i + 1)].T, dZ)
-            dA *= cache['D' + str(i)] / keep_prob  # Apply dropout
-            dZ = dA * (1 - np.tanh(cache['A' + str(i)])**2)
+    m = len(Y[0])
+    dz = cache['A'+str(L)] - Y
+    for i in range(L, 0, -1):
+        dw = (1/m) * np.matmul(dz, cache['A'+str(i-1)].T)
+        db = (1/m) * np.sum(dz, axis=1, keepdims=True)
+        da = np.matmul(weights['W'+str(i)].T, dz)
+        if i != 1:
+            drop = cache['D'+str(i-1)]
+            da *= drop
+            da /= keep_prob
+        dz = da * (1-cache['A'+str(i-1)]**2)
 
-        """Calculate the gradients for weights and biases"""
-        dW = (1 / m) * np.dot(dZ, cache['A' + str(i - 1)].T)
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-
-        """Update weights and biases"""
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
+        weights['W'+str(i)] = weights['W'+str(i)]\
+            - alpha * dw
+        weights['b'+str(i)] = weights['b'+str(i)]\
+            - alpha * db
