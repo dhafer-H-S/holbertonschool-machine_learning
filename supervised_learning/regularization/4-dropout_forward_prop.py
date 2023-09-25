6dfc9bd56f97385d996c6382af98a127c6390b80
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-25 10:37:08 +0200

    task-4-

---
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""a function that conducts forword propagation using dropout"""
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def dropout_forward_prop(X, weights, L, keep_prob):
+    """
+    X : containg the inpute data for the neural network
+    X shape(nx, m)
+    nx number of input feature
+    m is the number of data points
+
+    weights :  is a dictionary of the weights and biases of the neural network
+    L: number of layers
+    keep_prob : propability that node will kept
+    """
+    output_layer = {}
+    dropouts = {}
+    for i in range(1, L + 1):
+        if i < L:
+            Z = np.dot(weights['W'+str(i)], X) + weights['b'+str(i)]
+            activation = np.tanh(Z)
+            """activation with tanh activation function"""
+            dropout_mask = (np.random.rand(activation.shape[0], activation.shape[1]) < keep_prob)
+            """dropout"""
+            activation *= dropout_mask / keep_prob
+            output_layer[f'activation{i}'] = activation
+            dropout_mask[f'D{i}'] = dropout_mask
+
+            X = activation
+        else:
+            Z = np.dot(weights[f'W{i}'], X) + weights[f'b{i}']
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
+
+            output_layer[f'activation{i}'] = A
+    return {'output_layer': output_layer, 'dropout_mask': dropout_mask}
