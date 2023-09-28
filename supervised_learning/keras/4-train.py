16499d20072408b50d55ef2781d174e2ae969127
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 18:39:17 +0200

    task: train a model using mini batch gradient descent

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,13 +3,21 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        verbose=True,
+        shuffle=False):
     """
     network is the model to be trained
     data conating the inpute data of sahpe (m, nx)
     labels conating the labels of data of shape (m, classes)
     batch_size is the size of the batch used for mini batch gradient descent
-    verbose is a bloolean that deteminess id output should be printed during training
+    verbose is a bloolean that deteminess id output should be
+    printed during training
     shuffle is a boolean that detemines whether to shuffle the batches
     every epoch normaly it's a good idea
     """
