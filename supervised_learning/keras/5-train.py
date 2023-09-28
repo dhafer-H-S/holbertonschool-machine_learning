e6beb2fc04dbb6c78f9bb1485f5b3179f582dc3b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 18:44:37 +0200

    task: train a model using mini batch gradient descent to analyze validation data

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""train a model using mini batch gradient descent"""
+import tensorflow.keras as K
+
+
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        validation_data=None,
+        verbose=True,
+        shuffle=False):
+    """
+    network is the model to be trained
+    data conating the inpute data of sahpe (m, nx)
+    labels conating the labels of data of shape (m, classes)
+    batch_size is the size of the batch used for mini batch gradient descent
+    validation data is the data to validate the model with
+    verbose is a bloolean that deteminess id output should be
+    printed during training
+    shuffle is a boolean that detemines whether to shuffle the batches
+    every epoch normaly it's a good idea
+    """
+    model = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        validation_data=validation_data,
+        shuffle=shuffle,
+    )
+    return model
