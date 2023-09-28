246d1758c123ae36caa48b35a14c9d5819df26f9
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 11:54:04 +0200

    task:build neural netwok with keras using model pre defined function

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -8,14 +8,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     nx is the number of the inpute features to the network
     layer is the number of nodes in each layer of the netwrok
-    activation is a list containing the activation functionused for each layer of the network
+    activation is a list containing the activation functionused
+    for each layer of the network
     lambtha is th eL2 regularization parameter
     keep_prob is the propability that a node will kept for the dropout
     Returns: the keras model
     """
-
     """define the inpute layer"""
-    input  = K.layers.Input(shape=(nx,))
+    input = K.layers.Input(shape=(nx,))
     """
     connect the layers then creat a hiden layer as a dense
     that recive the inpute only form the inpute layer
@@ -24,16 +24,26 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         if i == 0:
             """this conditon check if the layer is the first layer or not"""
-            """if it's the first layer then the inpute layer is connected to it"""
-            hiden_layer = K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=regulaizer)(input)
+            """
+            if it's the first layer then the inpute layer is
+            connected to it
+            """
+            hiden_layer = K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regulaizer)(input)
         if i != len(layers) - 1:
             """this conditon check if the layer is the last layer or not"""
             """if it's not then a dropout layer is added after the curent layer"""
-            hiden_layer = K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=regulaizer)(hiden_layer)
+            hiden_layer = K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regulaizer)(hiden_layer)
             hiden_layer = K.layers.Dropout(1 - keep_prob)(hiden_layer)
             """1 - keep_prob is the propability that a node wil be dropped out """
         if i == layers[i]:
-            hiden_layer = K.layers.Dense(layers[i], activation='softmax')(hiden_layer)
+            hiden_layer = K.layers.Dense(
+                layers[i], activation='softmax')(hiden_layer)
     output = hiden_layer
     model = K.Model(inputs=input, outputs=output)
     return model
