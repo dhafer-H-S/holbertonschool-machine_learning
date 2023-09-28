72919c6c27d7be348932501029cbaea1d7d5315f
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 12:15:06 +0200

    task:build neural netwok with keras using model pre defined function

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -15,31 +15,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     Returns: the keras model
     """
     """define the inpute layer"""
-    input = K.layers.Input(shape=(nx,))
-    """
-    connect the layers then creat a hiden layer as a dense
-    that recive the inpute only form the inpute layer
-    """
-    regulaizer = K.regularizers.L2(lambtha)
-    for i in range(len(layers)):
-        """this conditon check if the layer is the first layer or not"""
-        """
-        if it's the first layer then the inpute layer is
-        connected to it
-        """
-        hiden_layer = K.layers.Dense(
-            layers[i],
-            activation=activations[i],
-            kernel_regularizer=regulaizer)(input)
+    prev = K.Input(shape=(nx,))
+    inputs = prev
+    l2 = K.regularizers.L2(lambtha)
+    for i, layer in enumerate(layers):
+        prev = K.layers.Dense(layer, activation=activations[i],
+                              kernel_regularizer=l2)(prev)
         if i != len(layers) - 1:
-            """this conditon check if the layer is the last layer or not"""
-            """
-            if it's not then a dropout layer is added after the curent layer
-            """
-            hiden_layer = K.layers.Dropout(1 - keep_prob)(hiden_layer)
-            """
-            1 - keep_prob is the propability that a node wil be dropped out
-            """
-    output = hiden_layer
-    model = K.Model(inputs=input, outputs=output)
+            prev = K.layers.Dropout(1-keep_prob)(prev)
+
+    model = K.Model(inputs=inputs, outputs=prev)
     return model
