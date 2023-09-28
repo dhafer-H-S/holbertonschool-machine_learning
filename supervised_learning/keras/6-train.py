6852e5d4653cd18e1d2ce09219a4e24afb50b63a
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 19:04:17 +0200

    task: train a model using mini batch gradient descent to analyze validation data and use early stopping

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""train a model using mini batch gradient descent and analyse validation data and use early stopping"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, verbose=True, shuffle=False):
+    """
+    network is the model to be trained
+    data conating the inpute data of sahpe (m, nx)
+    labels conating the labels of data of shape (m, classes)
+    batch_size is the size of the batch used for mini batch gradient descent
+    validation data is the data to validate the model with
+    early stoping is a boolean that indicates whether early stopping should be used
+    early stopping should only be performed if validation_data exists
+    early stopping should be based on validation loss
+    patience is the patience used for early stopping
+    verbose is a bloolean that deteminess id output should be
+    printed during training
+    shuffle is a boolean that detemines whether to shuffle the batches
+    every epoch normaly it's a good idea
+    """
+    model = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        validation_data=validation_data,
+        shuffle=shuffle,
+    )
+    return model.K.callbacks.EarlyStopping(monitor=early_stopping, patience=patience)
