4918a6c4be77791ac876e905344b657db42bed97
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-28 12:12:37 +0200

    task:build neural netwok with keras using model pre defined function

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -22,32 +22,24 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     regulaizer = K.regularizers.L2(lambtha)
     for i in range(len(layers)):
-        if i == 0:
-            """this conditon check if the layer is the first layer or not"""
-            """
-            if it's the first layer then the inpute layer is
-            connected to it
-            """
-            hiden_layer = K.layers.Dense(
-                layers[i],
-                activation=activations[i],
-                kernel_regularizer=regulaizer)(input)
+        """this conditon check if the layer is the first layer or not"""
+        """
+        if it's the first layer then the inpute layer is
+        connected to it
+        """
+        hiden_layer = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=regulaizer)(input)
         if i != len(layers) - 1:
             """this conditon check if the layer is the last layer or not"""
             """
             if it's not then a dropout layer is added after the curent layer
             """
-            hiden_layer = K.layers.Dense(
-                layers[i],
-                activation=activations[i],
-                kernel_regularizer=regulaizer)(hiden_layer)
             hiden_layer = K.layers.Dropout(1 - keep_prob)(hiden_layer)
             """
             1 - keep_prob is the propability that a node wil be dropped out
             """
-        if i == layers[i]:
-            hiden_layer = K.layers.Dense(
-                layers[i], activation='softmax')(hiden_layer)
     output = hiden_layer
     model = K.Model(inputs=input, outputs=output)
     return model
