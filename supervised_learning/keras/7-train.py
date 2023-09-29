f454f87938a0a6e9493fee014ee32d79adbc9ec2
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-09-29 16:11:30 +0200

    task: train a model using mini batch gradient descent to analyze validation data and use early stopping and to train the model with learning rate decay

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -6,20 +6,7 @@ and use early stopping and also train with learining rate decay
 import tensorflow.keras as K
 
 
-def train_model(
-        network,
-        data,
-        labels,
-        batch_size,
-        epochs,
-        validation_data=None,
-        early_stopping=False,
-        patience=0,
-        learning_rate_decay=False,
-        alpha=0.1,
-        decay_rate=1,
-        verbose=True,
-        shuffle=False):
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, learning_rate_decay=False, alpha=0.1, decay_rate=1, verbose=True, shuffle=False):
     """
     network is the model to be trained
     data containing the input data of shape (m, nx)
