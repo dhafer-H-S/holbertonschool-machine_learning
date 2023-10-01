bc2e5ec5fa639a621fb066c2ec64b3cfa52a9b1b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-02 00:45:15 +0200

    task: a save function that save model in a specific filename / a load function that load a model from a specific filename

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+a save function that save model in a specific filename 
+a load function that load a model from a specific filename
+"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    network is the model to save
+    filename is the path of the file that the model should be saved
+    """
+    K.saving.save_model(network, filename)
+    return None
+
+
+def load_model(filename):
+    """
+    filename is the path of the file that the model should be loaded
+    """
+    return K.saving.load_model(filename)
