83ae06c86706c176f33626095e2abe237e5c7548
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-02 01:55:53 +0200

    task: a save function that save weight of the model in a specific filename / a load function that load a weight of the model from a specific filename

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+function that saves weights and load 
+"""
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    network is the model whose weights should be saved
+    filename is the path of the file that the weights should be saved to
+    save_format is the format in which the weights should be saved
+    """
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+    """
+    network is the model to which the weights should be loaded
+    filename is the path of the file that the weights should be loaded from
+    """
+    network.load_weights(filename)
