054693ebf25d7a966ac48eabea0f53d42f21b41c
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-03 12:35:42 +0200

    task : a functionthat performs a valid convolution on grayscale images

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""a functionthat performs a valid convolution on grayscale images """
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    images with shape (m, h, w) containg multiple grayscale images
+    m number of images
+    h height in pixels of the images
+    w is the weidth on pixels of the images
+
+    kernel with shape of (kh, kw) containg the kernel for the convolution
+    kh is height of the kernelt
+    kw is the width of the kernel
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_h):
+        for j in range(output_h):
+            output[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw]).\
+                sum(axis=(1, 2))
+    return output
