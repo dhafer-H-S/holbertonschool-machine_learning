aa22e8a30ac5b1a5b83649122c7998f468211375
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-03 14:10:45 +0200

    task : a functionthat performs a valid convolution on grayscale images wityh condition for padding

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -16,16 +16,14 @@ def convolve_grayscale_same(images, kernel):
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    output_h = (h - kh) + 1
-    output_w = (w - kw) + 1
     padd_h = (kh - 1) // 2
     padd_w = (kw - 1) // 2 
     """calculate the padding needed"""
     pad = ((0, 0), (padd_h, padd_h), (padd_w, padd_w))
     images = np.pad(images, pad_width=pad, mode='constant')
-    output = np.zeros((m, output_h, output_w))
-    for i in range(0, output_h):
-        for j in range(0, output_w):
+    output = np.zeros(images.shape)
+    for i in range(0, h):
+        for j in range(0, w):
             output[:, i, j] = (kernel * images[:, i: i + kh,
                                j: j + kw]).sum(axis=(1, 2))
     return output
