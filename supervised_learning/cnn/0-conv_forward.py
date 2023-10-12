14681031258372f4f9e763ecc1bd8c149af2198f
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-12 17:40:59 +0200

    task : a function that performs forword propagation over a convolution layer of aneural network

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -6,7 +6,6 @@ over a convolution layer of aneural network
 import numpy as np
 
 
-
 def conv_forward(A, W, b, activation, padding="same", stride=(1, 1)):
     """
         A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
@@ -15,8 +14,8 @@ def conv_forward(A, W, b, activation, padding="same", stride=(1, 1)):
             h_prev is the height of the previous layer
             w_prev is the width of the previous layer
             c_prev is the number of channels in the previous layer
-        W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing kernels
-        for the convolution
+        W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+        kernels for the convolution
             kh is the filter height
             kw is the filter width
             c_prev is the number of channels in the previous layer
@@ -24,9 +23,10 @@ def conv_forward(A, W, b, activation, padding="same", stride=(1, 1)):
         b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
         applied to the convolution
         activation is an activation function applied to the convolution
-        padding is a string that is either same or valid, indicating the type of
-        padding used
-        stride is a tuple of (sh, sw) containing the strides for the convolution
+        padding is a string that is either same or valid, indicating the type
+        of padding used
+        stride is a tuple of (sh, sw) containing the strides for the
+        convolution
             sh is the stride for the height
             sw is the stride for the width
     """
@@ -42,7 +42,11 @@ def conv_forward(A, W, b, activation, padding="same", stride=(1, 1)):
     h_new = int((h_prev + 2 * ph - kh) / sh) + 1
     w_new = int((w_prev + 2 * pw - kw) / sw) + 1
     npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))
-    A_prev_padded = np.pad(A, pad_width=npad, mode='constant', constant_values=0)
+    A_prev_padded = np.pad(
+        A,
+        pad_width=npad,
+        mode='constant',
+        constant_values=0)
     output = np.zeros((m, h_new, w_new, c_new))
 
     for i in range(m):
@@ -51,7 +55,7 @@ def conv_forward(A, W, b, activation, padding="same", stride=(1, 1)):
                 for l in range(c_new):
                     output[i, j, k, l] = np.sum(
                         A_prev_padded[i, j * sh: j * sh + kh,
-                                    k * sw: k * sw + kw, :] * W[:, :, :, l])
+                                      k * sw: k * sw + kw, :] * W[:, :, :, l])
     A = activation(output + b)
 
     return A
