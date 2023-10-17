f849ce3ca035dd9d2d60f0b4340aa5309ae40bc5
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-17 08:15:00 +0200

    task : recreating LeNet 5 architcture from scratch

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -24,15 +24,15 @@ def lenet5(x, y):
     define sequential model
     """
     conv1 = tf.keras.layers.Conv2D(
-            fliters=6,
-            kernel_size=(
-                5,
-                5),
-            padding='same',
-            activation='tanh',
-            input_shape=x,
-            kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-            name='conv1')
+        fliters=6,
+        kernel_size=(
+            5,
+            5),
+        padding='same',
+        activation='tanh',
+        input_shape=x,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='conv1')
     """
     convolution layer 1ers
     """
@@ -41,45 +41,50 @@ def lenet5(x, y):
     max pooling layer 1
     """
     conv2 = tf.keras.layers.Conv2D(
-            fliters=16,
-            kernel_size=(5, 5),
-            padding='valid',
-            activation='tanh',
-            kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-            name='conv2')
+        fliters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation='tanh',
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='conv2')
     """
     convolution layer 2
     """
-    pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2')
+    pool2 = tf.keras.layers.MaxPooling2D(
+        pool_size=(
+            2, 2), strides=(
+            2, 2), name='pool2')
     """
     max pooling layer 2
     """
     fully_con = tf.keras.layers.Dense(
-            units=120,
-            activation='relu',
-            kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-            name='fully_con')
+        units=120,
+        activation='relu',
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='fully_con')
     """
     fully conected layer with 120 nodes
     """
     fully_con2 = tf.keras.layers.Dense(
-            units=84,
-            activation='relu',
-            kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-            name='fully_con2')
+        units=84,
+        activation='relu',
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='fully_con2')
     """
     fully conected layer with 84 nodes
     """
     fully_con3 = tf.keras.layers.Dense(
-            units=10,
-            activation='softmax',
-            kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-            name='fully_con3')
+        units=10,
+        activation='softmax',
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
+        name='fully_con3')
     """
     fully conected layer with 10 nodes and softmax activation
     """
 
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=fully_con3))
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits_v2(
+            labels=y, logits=fully_con3))
     """
     we calculate the loss using softmax cross entropy with logits v2"""
     optimizer = tf.train.AdamOptimizer()
