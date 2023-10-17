827fad29896894cdf0b862aa8e96d65ea77a7138
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-17 08:55:39 +0200

    task : recreating LeNet 5 architcture from scratch

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -7,90 +7,29 @@ import tensorflow.compat.v1 as tf
 
 
 def accuracy(Y, prediction):
-    """
-    function that calculates the accuracy of a prediction
-    """
-    Y = tf.argmax(Y, axis=1)
-    prediction = tf.argmax(prediction, axis=1)
-    correct_prediction = tf.equal(Y, prediction)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype='float'))
-    return accuracy
+    """Accuracy of prediction."""
+    y = tf.argmax(y, axis=1)
+    y_pred = tf.argmax(y_pred, axis=1)
+    correct = tf.equal(y, y_pred)
+    return tf.reduce_mean(tf.cast(correct, dtype='float'))
 
 
 def lenet5(X, Y):
-    """
-    function that builds a modified version of LeNet 5 architecture
-    """
-    m = X.shape
-    """
-    m is the number of images
-    """
-    conv1 = tf.layers.Conv2D(
-        filters=6,
-        kernel_size=(
-            5,
-            5),
-        padding='same',
-        activation='tanh',
-        input_shape=X,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='conv1')(X)
-    """
-    convolution layer 1ers
-    """
-    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
-    """
-    max pooling layer 1
-    """
-    conv2 = tf.layers.Conv2D(
-        filters=16,
-        kernel_size=(5, 5),
-        padding='valid',
-        activation='tanh',
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='conv2')(pool1)
-    """
-    convolution layer 2
-    """
-    pool2 = tf.layers.MaxPooling2D(
-        pool_size=(
-            2, 2), strides=(
-            2, 2), name='pool2')(conv2)
-    """
-    max pooling layer 2
-    """
-    flatten = tf.layers.Flatten()(pool2)
-    fully_con = tf.layers.Dense(
-        units=120,
-        activation='relu',
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='fully_con')(flatten)
-    """
-    fully conected layer with 120 nodes
-    """
-    fully_con2 = tf.layers.Dense(
-        units=84,
-        activation='relu',
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='fully_con2')(fully_con)
-    """
-    fully conected layer with 84 nodes
-    """
-    fully_con3 = tf.layers.Dense(
-        units=10,
-        activation='softmax',
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0),
-        name='fully_con3')(fully_con2)
-    """
-    fully conected layer with 10 nodes and softmax activation
-    """
-    output = tf.nn.softmax(fully_con3)
-    loss = tf.losses.softmax_cross_entropy_with_logits_v2(
-            onehot_labels=Y, logits=fully_con3)
-    """
-    we calculate the loss using softmax cross entropy with logits v2"""
+    init = tf.keras.initializers.VarianceScaling(scale=2.0)
+    x1 = tf.layers.Conv2D(filters=6, kernel_size=5, padding='same',
+                          kernel_initializer=init, activation='relu')(x)
+    px1 = tf.layers.MaxPooling2D(2, 2)(x1)
+    x2 = tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
+                          kernel_initializer=init, activation='relu')(px1)
+    px2 = tf.layers.MaxPooling2D(2, 2)(x2)
+    x_flat = tf.layers.Flatten()(px2)
+    cx1 = tf.layers.Dense(120, activation='relu',
+                          kernel_initializer=init)(x_flat)
+    cx2 = tf.layers.Dense(84, activation='relu', kernel_initializer=init)(cx1)
+    cx3 = tf.layers.Dense(10, kernel_initializer=init)(cx2)
+    y_pred = tf.nn.softmax(cx3)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=cx3)
     train_op = tf.train.AdamOptimizer().minimize(loss)
-    """ train operation using AdamOptimizer  """
-    accuracy = accuracy(Y, output)
-    """ we calculate the accuracy based on the function bellow """
-    return output, train_op, loss, accuracy
+    accuracy = calculate_accuracy(y, y_pred)
+
+    return y_pred, train_op, loss, accuracy
