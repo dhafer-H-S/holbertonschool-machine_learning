f4b56504059e338a5fea765c3ef5cc50d42412a7
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-25 17:51:55 +0200

    inception block creation

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -3,10 +3,9 @@
 import tensorflow.keras as K
 
 
-
 def inception_block(A_prev, filters):
     """function that builds inception block in a modified way"""
-    F1, F3R, F3,F5R, F5, FPP = filters
+    F1, F3R, F3, F5R, F5, FPP = filters
     conv1 = K.layers.Conv2D(filters=F1, kernel_size=1, padding='same',
                             activation='relu')(A_prev)
     conv3R = K.layers.Conv2D(filters=F3R, kernel_size=1, padding='same',
@@ -17,7 +16,12 @@ def inception_block(A_prev, filters):
                              activation='relu')(A_prev)
     conv5 = K.layers.Conv2D(filters=F5, kernel_size=5, padding='same',
                             activation='relu')(conv5R)
-    pool = K.layers.MaxPooling2D(pool_size=[3, 3], strides=1, padding='same')(A_prev)
+    pool = K.layers.MaxPooling2D(
+        pool_size=[
+            3,
+            3],
+        strides=1,
+        padding='same')(A_prev)
     convPP = K.layers.Conv2D(filters=FPP, kernel_size=1, padding='same',
                              activation='relu')(pool)
     output = K.layers.concatenate([conv1, conv3, conv5, convPP])
