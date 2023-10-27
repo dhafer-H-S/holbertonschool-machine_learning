d7cefeddd4b6d2222b50fe034373cc2b9127f555
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-10-27 13:07:24 +0200

    inception block creation

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -44,16 +44,14 @@ def inception_network():
     """inception block 6"""
     i7 = inception_block(i6, [112, 144, 288, 32, 64, 64])
     """inception block 7"""
-    i8 = inception_block(i7, [256, 160, 320, 32, 128, 128])
-    """inception block 8"""
     """max pooling layer 4"""
     M4pool = K.layers.MaxPool2D((3, 3), 2, padding='same')(i8)
+    i8 = inception_block(i7, [256, 160, 320, 32, 128, 128])
+    """inception block 8"""
     i9 = inception_block(M4pool, [256, 160, 320, 32, 128, 128])
     """inception block 9"""
-    i10 = inception_block(i9, [384, 192, 384, 48, 128, 128])
-    """inception block 10"""
     """average pooling layer"""
-    avg_pool = K.layers.AveragePooling2D((7, 7), strides=1)(i10)
+    avg_pool = K.layers.AveragePooling2D((7, 7), strides=1)(i9)
     """dropout layer"""
     dropout = K.layers.Dropout(0.4)(avg_pool)
     """softmax layer"""
