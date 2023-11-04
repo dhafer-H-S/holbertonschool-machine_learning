82ce7193850714c82d7ecc2bd4fb1ab7d3c7ce07
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-11-04 20:39:03 +0100

    transition layer

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" transition layer"""
+
+
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """transition layer 1 × 1 conv followed by 2 × 2 average pool, stride 2"""
+    init = K.initializers.he_normal(seed=None)
+    filters = int(nb_filters * compression)
+    layer = K.layers.BatchNormalization()(X)
+    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Conv2D(filters=filters,
+                            kernel_size=1,
+                            padding='same',
+                            kernel_initializer=init)(layer)
+    layer = K.layers.AveragePooling2D(pool_size=2,
+                                      strides=2,
+                                      padding='same')(layer)
+    return layer, filters
