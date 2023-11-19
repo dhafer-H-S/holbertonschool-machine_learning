095c33fcfe363beab10b4ec8acbc0655ed0cadba
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-11-19 01:06:28 +0100

    process the output of data

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -73,7 +73,6 @@ class Yolo():
             grid_h, grid_w, nb_box, _ = output.shape
             box_conf = sigmoid(output[:, :, :, 4])
             box_conf = box_conf.reshape(-1, 3)
-            box_conf = box_conf[..., np.newaxis]
             box_prob = sigmoid(output[:, :, :, 5:])
             box_confidences.append(box_conf)
             box_class_probs.append(box_prob)
