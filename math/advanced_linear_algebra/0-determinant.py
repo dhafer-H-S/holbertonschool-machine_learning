4dc0b61d665969bb31f09f69f126bf883b09298e
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-11-27 17:04:25 +0100

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,22 +3,31 @@
 
 
 def determinant(matrix):
-    """Calculates the determinant of a matrix"""
+    """calculates the determinant of a matrix"""
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    if not all([isinstance(row, list) for row in matrix]):
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]) and len(matrix[0]) > 1:
+    for sub_list in matrix:
+        if not isinstance(sub_list, list):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix[0]) == 0:
+        return 1
+
+    if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    if len(matrix[0]) == 1:
+
+    if len(matrix) == 1:
         return matrix[0][0]
-    if len(matrix[0]) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
 
     det = 0
-    for i, k in enumerate(matrix[0]):
-        rows = [row for row in matrix[1:]]
-        aux_matrix = [[row[n] for n in range(
-            len(matrix)) if n != i] for row in rows]
-        det += k * (-1) ** i * determinant(aux_matrix)
+    for idx, element in enumerate(matrix[0]):
+        inner = []
+        for row in matrix[1:]:
+            inner.append(row[:idx] + row[idx + 1:])
+        sign = (-1) ** idx
+        det += sign * element * determinant(inner)
+
     return det
