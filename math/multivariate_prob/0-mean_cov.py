cda744735bf9787df3f393fb5a4dc81961f2c258
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-12-05 17:14:25 +0100

    mean and covarience : a fucntion that calculates the covariance of a matrix

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -14,13 +14,9 @@ def mean_cov(X):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    if X.shape[0] != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
     n, d = X.shape
-    if n < 2:
-        raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
     X = X - mean
     cov = np.matmul(X.T, X) / (n - 1)
