1ebc3c1a5f0a45845480f0ad3eb0ea1d8c5ccd18
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-12-06 15:42:03 +0100

    a fucntion that calculates the correlation of a matrix

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,9 +7,14 @@ def correlation(C):
     """function that calculates the correlation of a matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if len(C.shape) != 2:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
+
     D = np.sqrt(np.diag(C))
-    correlation_matrix = C / (D * D[:, None])
+    correlation_matrix = np.zeros(C.shape)
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            correlation_matrix[i][j] = C[i][j] / (D[i] * D[j])
 
     return correlation_matrix
