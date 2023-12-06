93f8834a2796c5a96294d59e11b44de5b20352a5
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-12-06 17:36:48 +0100

    a class that represnet a multivariate normal distribustion

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -9,7 +9,7 @@ class MultiNormal:
 
     def __init__(self, data):
         """function that initialize data"""
-        if not isinstance(data, np.ndarray):
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
         d, n = data.shape
         """
@@ -19,6 +19,5 @@ class MultiNormal:
         if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
-        d = data.shape[1]
         """cov is of shape (d, d) like if (d, d) = (x, y)"""
         self.cov = ((data - self.mean) @ (data - self.mean).T) / (n - 1)
