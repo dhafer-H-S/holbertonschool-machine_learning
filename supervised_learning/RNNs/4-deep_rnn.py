445b6654dd175d2693bc973607b7ee86071a2d72
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-12-19 16:52:50 +0100

    deep RNN function tha performs forward propagation for a deep RNN

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -5,13 +5,16 @@ import numpy as np
 
 def deep_rnn(rnn_cells, X, h_0):
     """
-    rnn_cells is a list of RNNCell instances of length l that will be used for the forward propagation
+    rnn_cells is a list of RNNCell instances of length l that
+    will be used for the forward propagation
     l is the number of layers
-    X is the data to be used, given as a numpy.ndarray of shape (t, m, i)
+    X is the data to be used, given as a numpy.ndarray of
+    shape (t, m, i)
     t is the maximum number of time steps
     m is the batch size
     i is the dimensionality of the data
-    h_0 is the initial hidden state, given as a numpy.ndarray of shape (l, m, h)
+    h_0 is the initial hidden state, given as a
+    numpy.ndarray of shape (l, m, h)
     h is the dimensionality of the hidden state
     """
     t, m, i = X.shape
