dff7a6dda909ba07a397449b92bbc4c45c875b47
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2023-12-28 12:14:11 +0100

    RNN

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -6,10 +6,9 @@ import numpy as np
 def rnn(rnn_cell, X, h_0):
     """RNN simple function"""
     t, m, i = X.shape
-    h = h_0.shape[1]
-    o = rnn_cell.by.shape[1]
+    _, h = h_0.shape
     H = np.zeros((t + 1, m, h))
-    Y = np.zeros((t, m, o))
+    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
     H[0] = h_0
     for i in range(t):
         h_next, y = rnn_cell.forward(H[i], X[i])
