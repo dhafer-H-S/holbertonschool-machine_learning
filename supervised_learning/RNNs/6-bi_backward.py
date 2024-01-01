2e37d4281b4e5fc2ab2f00c39f49e4c2b551fe85
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-01-01 17:50:54 +0100

    BidirectionalCell that represents a bidirectional cell of an RNN with forward and backward propagation

---
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+biderctional RNN function that performs
+forward and backward propagation for a bidirectional RNN
+"""
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """class that represents a bidirectional cell of an RNN"""
+
+    def __init__(self, i, h, o):
+        """initialization"""
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h + h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """function that performs forward propagation for one time step"""
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concat, self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """function that performsa backward propagtion for one time step"""
+        concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.matmul(concat, self.Whb) + self.bhb)
+        return h_prev
