c22ea60578d5b707e8b9d1d5f0b62c91c381488b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-01-14 23:58:03 +0100

    wod of bag function

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -2,17 +2,26 @@
 """woord of bag"""
 
 import numpy as np
+import re
+
 
 def bag_of_words(sentences, vocab=None):
+    """Creates a bag of words embedding matrix"""
+    def clean_sentence(sentence):
+        """Clean sentence before embedding"""
+        return re.sub(r"\b\w{1}\b", "", re.sub(
+            r"[^a-zA-Z0-9\s]", " ", sentence.lower())).split()
+
     if vocab is None:
-        features = sorted(set(word for sentence in sentences for word in sentence.split()))
-    else:
-        features = sorted(vocab)
-    
-    embeddings = np.zeros((len(sentences), len(features)))
-    
+        vocab = sorted(
+            set(word for sentence in sentences for word in clean_sentence(sentence)))
+
+    embeddings = np.zeros((len(sentences), len(vocab)))
+
     for i, sentence in enumerate(sentences):
-        for j, word in enumerate(features):
-            embeddings[i, j] = sentence.split().count(word)
-    
-    return embeddings, features
+        cleaned_sentence = clean_sentence(sentence)
+        for word in cleaned_sentence:
+            if word in vocab:
+                embeddings[i, vocab.index(word)] += 1
+
+    return embeddings.astype(int), vocab
