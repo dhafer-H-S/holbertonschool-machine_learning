b8cd52c530ee700b8922ea992cb6837343f862de
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-01-17 13:04:20 +0100

    tf-idf

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""tf-idf"""
+
+import numpy as np
+import re
+
+
+def tf_idf(sentences, vocab=None):
+    """tf-idf"""
+    def clean_data(sentence):
+        return re.sub(r"\b\w{1}\b", "", re.sub(
+            r"[^a-zA-Z0-9\s]", " ", sentence.lower())).split()
+    if vocab is None:
+        vocab = []
+        for sentence in sentences:
+            for word in clean_data(sentence):
+                vocab.append(word)
+
+    embeddings = np.zeros((len(sentences), len(vocab)))
+    for i, sentence in enumerate(sentences):
+        for word in vocab:
+            tf = ((sentence.count(word)) / len(sentence))
+            idf = np.log(len(sentences) /
+                         (sum(word in s for s in sentences) + 1e-10))
+            tf_idf = tf * idf
+            embeddings[i, vocab.index(word)] = tf_idf
+    return embeddings, vocab
