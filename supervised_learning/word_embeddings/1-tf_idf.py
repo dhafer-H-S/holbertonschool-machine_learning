e91f8b06ff05980499249cde425b61ace82b11c0
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-01-18 11:19:17 +0100

    tf-idf

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python3
-"""tf-idf"""
-
+"""0. Bag Of Words"""
 import numpy as np
-import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    """tf-idf"""
-    def clean_data(sentence):
-        return re.sub(r"\b\w{1}\b", "", re.sub(
-            r"[^a-zA-Z0-9\s]", " ", sentence.lower())).split()
-    if vocab is None:
-        vocab = []
-        for sentence in sentences:
-            for word in clean_data(sentence):
-                vocab.append(word)
-
-    embeddings = np.zeros((len(sentences), len(vocab)))
-    for i, sentence in enumerate(sentences):
-        for word in vocab:
-            tf = ((sentence.count(word)) / len(sentence))
-            idf = np.log(len(sentences) /
-                         (sum(word in s for s in sentences)))
-            tf_idf = tf * idf
-            embeddings[i, vocab.index(word)] = tf_idf
-    return embeddings, vocab
+    """creates a TF-IDF embedding"""
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return X.toarray(), vectorizer.get_feature_names_out()
