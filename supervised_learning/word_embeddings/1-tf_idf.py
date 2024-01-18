7df1eeef078f566af85d3b06277836f3d7289ffd
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-01-18 16:07:38 +0100

    tf-idf

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
-    """creates a TF-IDF embedding"""
+    """TF-IDF"""
     vectorizer = TfidfVectorizer(vocabulary=vocab)
     X = vectorizer.fit_transform(sentences)
     return X.toarray(), vectorizer.get_feature_names_out()
