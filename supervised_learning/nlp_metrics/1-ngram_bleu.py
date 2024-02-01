3ad3290bb36fc5535b0c74a40b948339e408a8cc
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-01 16:55:42 +0100

    ngram blue score

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -13,24 +13,34 @@ def ngram_bleu(references, sentence, n):
     n is the number of n gram to use for evoluation
     """
    # Create n-grams for sentence
-    sentence_ngrams = Counter([tuple(sentence[i:i+n]) for i in range(len(sentence)-n+1)])
+    sentence_ngrams = Counter([tuple(sentence[i:i + n])
+                              for i in range(len(sentence) - n + 1)])
 
     max_counts = {}
     for reference in references:
         # Create n-grams for reference
-        reference_ngrams = Counter([tuple(reference[i:i+n]) for i in range(len(reference)-n+1)])
+        reference_ngrams = Counter(
+            [tuple(reference[i:i + n]) for i in range(len(reference) - n + 1)])
         for ngram in sentence_ngrams:
-            max_counts[ngram] = max(max_counts.get(ngram, 0), reference_ngrams[ngram])
+            max_counts[ngram] = max(max_counts.get(
+                ngram, 0), reference_ngrams[ngram])
 
-    clipped_counts = {ngram: min(count, max_counts.get(ngram, 0)) for ngram, count in sentence_ngrams.items()}
+    clipped_counts = {
+        ngram: min(
+            count,
+            max_counts.get(
+                ngram,
+                0)) for ngram,
+        count in sentence_ngrams.items()}
 
     bleu_score = sum(clipped_counts.values()) / max(len(sentence_ngrams), 1)
-    
+
     # Calculate brevity penalty
-    closest_ref_len = min((abs(len(sentence) - len(ref)), len(ref)) for ref in references)[1]
+    closest_ref_len = min((abs(len(sentence) - len(ref)), len(ref))
+                          for ref in references)[1]
     if len(sentence) > closest_ref_len:
         brevity_penalty = 1
     else:
         brevity_penalty = np.exp(1 - closest_ref_len / len(sentence))
-    
+
     return brevity_penalty * bleu_score
