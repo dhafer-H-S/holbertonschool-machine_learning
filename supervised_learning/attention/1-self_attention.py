9736b40f9b8d5348766df89723ca8b04cecd15a3
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-12 16:31:50 +0100

    self attention

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,37 +1,32 @@
-#!/usr/bin/env python3
-"""self attention"""
-
+"""attention"""
 import tensorflow as tf
 
 
 class SelfAttention(tf.keras.layers.Layer):
-    """a self attention class"""
+    """calculate attention for machine translation"""
 
     def __init__(self, units):
+        """constructor
+        units is an integer representing the number of hidden units in the
+        alignment model"""
         super(SelfAttention, self).__init__()
-        """
-        units is a number representing the number of hidden units in the
-        alignement model
-        """
-        self.W = tf.keras.layers.Dense(units=units, activation='relu')
-        """decoder hidden state transformation"""
-        self.U = tf.keras.layers.Dense(units=units, activation='relu')
-        """encoder hidden states transformation"""
-        self.V = tf.keras.layers.Dense(units=1)
-        """attention scoring"""
+        # W is hidden state of encoder
+        self.W = tf.keras.layers.Dense(units)
+        # U is hidden state of decoder
+        self.U = tf.keras.layers.Dense(units)
+        # V is the tanh of the sum of the outputs of W and U
+        self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
-        """
-        call function to calcualte attention weights
+        """calls the attention
         s_prev is a tensor of shape (batch, units) containing the previous
         decoder hidden state
-        hidden_states is a tensor of shape (batch, inut_seq_len, units)
-        conatining the output of the encoder
-        """
-        score = self.V(tf.nn.tanh(self.W(tf.expand_dims(s_prev, 1)) + self.U(hidden_states)))
-        attention_weights = tf.nn.softmax(score, axis=1)
-        attention_weights = tf.expand_dims(attention_weights, -1)
-        context_vector = attention_weights * hidden_states
-        context_vector = tf.reduce_sum(context_vector, axis=1)
-
-        return context_vector, attention_weights
+        hidden_states is a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the encoder
+        Returns: context, weights"""
+        s_prev = tf.expand_dims(s_prev, 1)
+        score = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
+        weights = tf.nn.softmax(score, axis=1)
+        context = weights * hidden_states
+        context = tf.reduce_sum(context, axis=1)
+        return context, weights
