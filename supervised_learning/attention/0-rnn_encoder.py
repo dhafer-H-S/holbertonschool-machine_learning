4f3106ea85a0ec3f92ec3ef05e1a0c24abfb45e2
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-12 02:59:59 +0100

    rnn encoder

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -49,23 +49,23 @@ class RNNEncoder:
         """
         return tf.zeros([self.batch, self.units])
 
-    def call(self, x, initial):
-        """
-        Perform the forward pass of the RNN Encoder.
-        Args:
-            x (tf.Tensor): Input tensor of shape
-            (batch_size, sequence_length, input_dim).
-            initial (tf.Tensor): Initial state tensor of shape
-            (batch_size, units).
-        Returns:
-            outputs (tf.Tensor): Output tensor of shape
-            (batch_size, sequence_length, units).
-            state (tf.Tensor): Final state tensor of shape
-            (batch_size, units).
-        """
-        if len(x.shape) == 2:
-            x = tf.reshape(x, (x.shape[0], 1, x.shape[1]))
-        if initial is None:
-            initial = tf.zeros((x.shape[0], self.units))
-        outputs, state = self.gru(x, initial)
-        return outputs, state
+def call(self, x, initial):
+    """
+    Perform the forward pass of the RNN Encoder.
+    Args:
+        x (tf.Tensor): Input tensor of shape
+        (batch_size, sequence_length, input_dim).
+        initial (tf.Tensor): Initial state tensor of shape
+        (batch_size, units).
+    Returns:
+        outputs (tf.Tensor): Output tensor of shape
+        (batch_size, sequence_length, units).
+        state (tf.Tensor): Final state tensor of shape
+        (batch_size, units).
+    """
+    if len(x.shape) == 2:
+        x = tf.reshape(x, (x.shape[0], 1, x.shape[1]))
+    if initial is None:
+        initial = tf.zeros((x.shape[0], self.units))
+    outputs, state = self.gru(x, initial_state=[initial])
+    return outputs, state
