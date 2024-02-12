529f870e6e5eb7a3217ac8177b55a0fbbe482cc1
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-12 02:39:58 +0100

    rnn encoder

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -5,17 +5,24 @@ import tensorflow as tf
 
 class RNNEncoder:
     """creat a class recurent neural network"""
+
     def __init__(self, vocab, embedding, units, batch):
         """
         vocab is an integer representing the size of the inpute vocabulary
-        embedding is an integer representing the dimensionality of the embedding vector
-        units is an integer representing the number of hidden units in the rnn cell
+        embedding is an integer representing the dimensionality of 
+        the embedding vector
+        units is an integer representing the number of hidden units in 
+        the rnn cell
         batch is an integer representing the batch size
         """
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform')
 
     def initialize_hidden_state(self):
         """
