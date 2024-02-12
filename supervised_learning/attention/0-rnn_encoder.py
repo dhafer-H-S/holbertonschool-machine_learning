331ea6525f4cd40892f577147afa4fd13bb83562
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-12 02:47:56 +0100

    rnn encoder

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -4,16 +4,30 @@ import tensorflow as tf
 
 
 class RNNEncoder:
-    """creat a class recurent neural network"""
+    """
+    A class representing a Recurrent Neural Network Encoder.
+
+    Attributes:
+        vocab (int): The size of the input vocabulary.
+        embedding (int): The dimensionality of the embedding vector.
+        units (int): The number of hidden units in the RNN cell.
+        batch (int): The batch size.
+
+    Methods:
+        __init__(self, vocab, embedding, units, batch): Initializes the RNNEncoder object.
+        initialize_hidden_state(self): Initializes the hidden state to zeros.
+        call(self, x, initial): Extracts input sequences.
+    """
 
     def __init__(self, vocab, embedding, units, batch):
         """
-        vocab is an integer representing the size of the inpute vocabulary
-        embedding is an integer representing the dimensionality of 
-        the embedding vector
-        units is an integer representing the number of hidden units in 
-        the rnn cell
-        batch is an integer representing the batch size
+        Initializes the RNNEncoder object.
+
+        Args:
+            vocab (int): The size of the input vocabulary.
+            embedding (int): The dimensionality of the embedding vector.
+            units (int): The number of hidden units in the RNN cell.
+            batch (int): The batch size.
         """
         self.batch = batch
         self.units = units
@@ -26,13 +40,23 @@ class RNNEncoder:
 
     def initialize_hidden_state(self):
         """
-        fuction to initialize the the hiden state to zeros
+        Initializes the hidden state to zeros.
+
+        Returns:
+            tf.Tensor: The initialized hidden state.
         """
         return tf.zeros([self.batch, self.units])
 
-    def call(self, x, initial):
+    def __call__(self, x, initial):
         """
-        function to extract inpute sequences
+        Extracts input sequences.
+
+        Args:
+            x (tf.Tensor): The input tensor.
+            initial (tf.Tensor): The initial hidden state.
+
+        Returns:
+            tuple: A tuple containing the outputs and the hidden state.
         """
         x = self.embedding(x)
         outputs, hidden = self.gru(x, initial_state=initial)
