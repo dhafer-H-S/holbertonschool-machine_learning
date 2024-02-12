6066a0df836af7002b6dc9d64d226a50427526b6
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-02-12 15:47:35 +0100

    self attention

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""self attention"""
+
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """a self attention class"""
+
+    def __init__(self, units):
+        super(SelfAttention, self).__init__()
+        """
+        units is a number representing the number of hidden units in the
+        alignement model
+        """
+        self.W = tf.keras.layers.Dense(units=units, activation='relu')
+        """decoder hidden state transformation"""
+        self.U = tf.keras.layers.Dense(units=units, activation='relu')
+        """encoder hidden states transformation"""
+        self.V = tf.keras.layers.Dense(units=1)
+        """attention scoring"""
+
+    def call(self, s_prev, hidden_states):
+        """
+        call function to calcualte attention weights
+        s_prev is a tensor of shape (batch, units) containing the previous
+        decoder hidden state
+        hidden_states is a tensor of shape (batch, inut_seq_len, units)
+        conatining the output of the encoder
+        """
+        score = self.V(
+            tf.nn.tanh(
+                self.W(
+                    tf.expand_dims(
+                        s_prev,
+                        1)) +
+                self.U(hidden_states)))
+        attention_weights = tf.nn.softmax(score, axis=1)
+        context_vector = attention_weights * hidden_states
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
