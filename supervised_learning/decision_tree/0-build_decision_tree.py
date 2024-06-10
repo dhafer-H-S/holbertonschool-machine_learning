8751f0c2bd73670b3db883eedece6806139a1ce7
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-06-10 11:55:24 +0100

    max depth of a tree

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -5,6 +5,21 @@ import numpy as np
 
 
 class Node:
+    """
+    This class represents a node in a decision tree.
+
+    A node holds the feature and threshold used for splitting the data, 
+    references to the left and right child nodes, and other properties 
+    related to the node's role in the tree.
+
+    Attributes:
+        feature (int): The index of the feature used for splitting the data at this node.
+        threshold (float): The threshold value used for splitting the data at this node.
+        left_child (Node): The left child node.
+        right_child (Node): The right child node.
+        is_root (bool): Indicates whether this node is the root of the decision tree.
+        depth (int): The depth of the node in the decision tree.
+    """
     def __init__(
             self,
             feature=None,
@@ -66,6 +81,15 @@ class Node:
 
 
 class Leaf(Node):
+    """
+    Represents a leaf node in a decision tree.
+
+    Attributes:
+        value (any): The value associated with the leaf node.
+        is_leaf (bool): Indicates whether the node is a leaf node.
+        depth (int): The depth of the leaf node in the decision tree.
+    """
+
     def __init__(self, value, depth=None):
         super().__init__()
         self.value = value
@@ -73,10 +97,30 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
+        """
+        Returns the maximum depth below the leaf node.
+
+        Returns:
+            int: The maximum depth below the leaf node.
+        """
         return self.depth
 
 
 class Decision_Tree():
+    """
+    Decision_Tree class represents a decision tree model.
+
+    Attributes:
+        max_depth (int): The maximum depth of the decision tree. Default is 10.
+        min_pop (int): The minimum number of samples required to split a node. Default is 1.
+        seed (int): The seed value for random number generation. Default is 0.
+        split_criterion (str): The criterion used to split the nodes. Default is "random".
+        root (Node): The root node of the decision tree. Default is None.
+
+    Methods:
+        depth(): Returns the maximum depth of the decision tree.
+    """
+
     def __init__(
             self,
             max_depth=10,
@@ -97,4 +141,10 @@ class Decision_Tree():
         self.predict = None
 
     def depth(self):
+        """
+        Returns the maximum depth of the decision tree.
+
+        Returns:
+            int: The maximum depth of the decision tree.
+        """
         return self.root.max_depth_below()
