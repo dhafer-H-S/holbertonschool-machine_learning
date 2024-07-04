8c265219090caa85a0c9449b039e0cf1b0d22fe2
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:24:49 +0100

    updates

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-
+"""line"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
+    """line ploting"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, 'r-')
