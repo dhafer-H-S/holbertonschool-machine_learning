848cdac7f335583a552b6f6a00c6252cf27ed34d
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:24:28 +0100

    updates

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    y = np.arange(0, 11) ** 3
 
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, 'r-')
     plt.axis([0, 10, 0, 1000])
     plt.show()
