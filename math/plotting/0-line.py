acd4966bf46354ebceaf24a0f9d37b8e00065a2b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:45:58 +0100

    updates

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-"""Line Graph"""
-
+"""0. Line Graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
     """plot y as a line graph"""
-    x = np.arange(0, 11)
+    y = np.arange(0, 11) ** 3
+    print(y)
     plt.figure(figsize=(6.4, 4.8))
-    y = x ** 3
-    plt.plot(x, y, color='red')
+    plt.plot(y, color='red')
     plt.xlim(0, 10)
-    plt.ylim(0, 1000)
-    plt.xlabel('x-axis')
-    plt.ylabel('y-axis')
-    plt.title('Cubic Line Graph')
-    plt.grid(True)
-    plt.tight_layout()
     plt.show()
