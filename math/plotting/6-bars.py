3e1f9cecaf6b500fd305cdcad0cb751ef4df4a37
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 21:33:57 +0100

    update versions

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,25 +2,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4, 3))
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
 
-# your code here
-fig, ax = plt.subplots()
-people = ["Farrah", "Fred", "Felicia"]
-colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-fruits = ['apples', 'bananas', 'oranges', 'peaches']
+    fig, ax = plt.subplots()
+    people = ["Farrah", "Fred", "Felicia"]
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    fruits = ['apples', 'bananas', 'oranges', 'peaches']
 
-bottom = np.zeros(len(people))
+    bottom = np.zeros(len(people))
 
-for i, row in enumerate(fruit):
-    ax.bar(people, row, bottom=bottom,
-           color=colors[i], label=fruits[i], width=0.5)
-    bottom += row
+    for i, row in enumerate(fruit):
+        ax.bar(people, row, bottom=bottom,
+            color=colors[i], label=fruits[i], width=0.5)
+        bottom += row
 
-ax.set_ylabel('Quantity of Fruit')
-ax.set_title('Number of Fruit per Person')
-ax.set_ylim([0, 80])
-ax.set_yticks(np.arange(0, 81, 10))
-ax.legend()
-plt.show()
+    ax.set_ylabel('Quantity of Fruit')
+    ax.set_title('Number of Fruit per Person')
+    ax.set_ylim([0, 80])
+    ax.set_yticks(np.arange(0, 81, 10))
+    ax.legend()
+    plt.show()
