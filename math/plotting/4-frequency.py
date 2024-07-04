d557ed9a0ddfc587a3331127d0215461c79a5701
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:14:12 +0100

    updates

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,8 +8,7 @@ def frequency():
     """
     Plot a histogram of student grades.
 
-    This function generates a random set of student grades
-    and plots a histogram
+    This function generates a random set of student grades and plots a histogram
     to visualize the distribution of grades.
 
     Args:
@@ -20,11 +19,11 @@ def frequency():
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
     bins_range = np.arange(start=int(min(student_grades) / 10) * 10,
                            stop=(int(max(student_grades) / 10) + 1) * 10,
                            step=10)
     counts, _ = np.histogram(student_grades, bins=bins_range)
-    plt.figure(figsize=(6.4, 4.8))
     plt.bar(bins_range[:-1], counts, width=9, edgecolor='black', align='edge')
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
