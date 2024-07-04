f15a31e881754d1eb1abc3b9a376d91a8d09b7a1
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:37:45 +0100

    updates

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 def line():
     """plot y as a line graph"""
     x = np.arange(0, 11)
+    plt.figure(figsize=(6.4, 4.8))
     y = x ** 3
     print(y)
-    plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y, color='red')
     plt.xlim(0, 10)
     plt.ylim(0, 1000)
