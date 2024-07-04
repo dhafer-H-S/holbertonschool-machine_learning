50d4f08e119a68bf7ff3b5346148c58403664007
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-04 22:17:00 +0100

    updates

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python3
-"""import modules"""
+"""4. Frequency"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
     """
-    Plot a histogram of student grades.
-
-    This function generates a random set of student grades and plots a histogram
-    to visualize the distribution of grades.
-
-    Args:
-        None
-
-    Returns:
-        None
+    plot a histogram of student scores for a project
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    bins_range = np.arange(start=int(min(student_grades) / 10) * 10,
-                           stop=(int(max(student_grades) / 10) + 1) * 10,
-                           step=10)
-    counts, _ = np.histogram(student_grades, bins=bins_range)
-    plt.bar(bins_range[:-1], counts, width=9, edgecolor='black', align='edge')
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
-    plt.xticks(bins_range)
+    plt.title('Project A')
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.xlim(0, 100)
+    plt.ylabel('Number of Students')
+    plt.ylim(0, 30)
+    plt.xticks(np.arange(0, 101, 10))
     plt.show()
-
-frequency()
