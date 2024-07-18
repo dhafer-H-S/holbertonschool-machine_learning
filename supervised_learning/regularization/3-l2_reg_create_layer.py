343d8bb16e1b08bc4639c46fb2eff55ae188449c
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 03:53:03 +0100

    update code l2 create layer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ a tensorflow layer that includes L2 regularization """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
@@ -10,15 +10,15 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     activation: is the activation function that should be used on the layer
     lambtha: the L2 regularization parameter
     """
-    L2_regularization = tf.keras.regularizers.L2(lambtha)
+    L2_regularization = tf.compat.v1.keras.regularizers.L2(lambtha)
     """a regularier that applies a L2 regularization penalty"""
-    layer_weight = tf.keras.initializers.VarianceScaling(
+    layer_weight = tf.compat.v1.keras.initializers.VarianceScaling(
         scale=2.0, mode=("fan_avg"))
     """
     initializer that adapt tits scale tot the shape og it's inpute tensors
     """
 
-    layer = tf.layers.Dense(n, activation=activation,
+    layer = tf.compat.v1.layers.Dense(n, activation=activation,
                             kernel_initializer=layer_weight,
                             kernel_regularizer=L2_regularization)
     return layer(prev)
