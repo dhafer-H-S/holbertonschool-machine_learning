10f0b0dd8103c74ee2b9cda6dbfa2a94cc8a7474
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 04:02:23 +0100

    update code task 6 dropout

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -15,7 +15,7 @@ def dropout_create_layer(prev, n, activation, keep_prob):
 
     weight = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                    mode="fan_avg")
-    layer = tf.layers.Dense(unites=n, activation=activation,
+    layer = tf.keras.layers.Dense(unites=n, activation=activation,
                             kernel_initializer=weight)
-    dlayer = tf.layers.Dropout(keep_prob)
+    dlayer = tf.keras.layers.Dropout(keep_prob)
     return dlayer(layer(prev))
