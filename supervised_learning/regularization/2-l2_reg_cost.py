6272114a9d83223335bf4a888bd6a23f878e81fd
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 03:38:24 +0100

    update file

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ calculate cost function with L2 regularization """
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def l2_reg_cost(cost):
@@ -8,5 +8,5 @@ def l2_reg_cost(cost):
     cost is a tensor containg the cost of the network without
     L2 regularization
     """
-    l2_reg = tf.losses.get_regularization_losses()
+    l2_reg = tf.compat.v1.losses.get_regularization_losses()
     return cost + l2_reg
