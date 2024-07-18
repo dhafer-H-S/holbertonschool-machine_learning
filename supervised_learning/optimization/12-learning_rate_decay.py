61c34dbc864cbae2affec88f98f4211ecbafcfd0
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 04:40:15 +0100

    12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -2,7 +2,7 @@
 """ decay operation using tensorflow predefined function """
 
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
