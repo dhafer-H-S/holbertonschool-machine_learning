0cb0e361be0a7924320c69160a9cae0bc9ebf353
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 04:53:53 +0100

    10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -2,14 +2,14 @@
 """ adam optimizer using tenserflow predefined function"""
 
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """ adam optimizer using tenserflow predefined function"""
     adam_optimizer = tf.compat.v1.train.AdamOptimizer(
         learning_rate=alpha,
         beta1=beta1,
         beta2=beta2,
         epsilon=epsilon,)
-    return adam_optimizer.minimize(loss)
+    return adam_optimizer
