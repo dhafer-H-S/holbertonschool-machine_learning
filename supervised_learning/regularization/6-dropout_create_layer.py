a4c8713842f06c26bb4115c9b91a466fbbf26cdb
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 03:59:34 +0100

    update code task 6 dropout

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Create Dropout layer in tensorflow"""
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
@@ -15,7 +15,7 @@ def dropout_create_layer(prev, n, activation, keep_prob):
 
     weight = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                    mode="fan_avg")
-    layer = tf.layers.Dense(n, activation=activation,
+    layer = tf.layers.Dense(unites=n, activation=activation,
                             kernel_initializer=weight)
     dlayer = tf.layers.Dropout(keep_prob)
     return dlayer(layer(prev))
