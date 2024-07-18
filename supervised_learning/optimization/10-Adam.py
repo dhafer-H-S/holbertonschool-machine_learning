c321072e6d91f0d7fe28cc9dcf0b9e496d9ae4c8
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 04:57:39 +0100

    10

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def create_Adam_op(alpha, beta1, beta2, epsilon):
     """ adam optimizer using tenserflow predefined function"""
-    optimizer = tf.compat.v1.train.AdamOptimizer(
+    optimizer = tf.keras.optimizers.Adam(
         learning_rate=alpha,
         beta1=beta1,
         beta2=beta2,
