000032cafa6fdcfc5883a13d88c8ccdf234bd0d7
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 04:50:51 +0100

    12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -6,8 +6,10 @@ import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, decay_step):
-    """ function to calculate decay operation using
-    tensorflow predefined function """
+    """
+    function to calculate decay operation using
+    tensorflow predefined function
+    """
 
     decay = tf.keras.optimizers.schedules.InverseTimeDecay(
         learning_rate=alpha,
