60acc2b86d0bc5209b2c1b1146a419b718ecff6a
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-18 05:30:41 +0100

    3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -10,6 +10,22 @@ function trains a loaded neural network model using mini batch gradient descent
 
 
 def create_mini_batches(X, Y, batch_size):
+    """
+    Creates mini-batches from the given dataset.
+
+    Args:
+        X (numpy.ndarray): The input data of shape (m, nx)
+        where m is the number of data points and nx is
+        the number of features.
+        Y (numpy.ndarray): The labels of shape (m, ny)
+        where m is the number of data points and ny is
+        the number of classes.
+        batch_size (int): The size of each mini-batch.
+
+    Returns:
+        list: A list of mini-batches, where each mini-batch
+        is a tuple containing the input data and labels.
+    """
     x_shuffled, y_shuffled = shuffle_data(X, Y)
     m = X.shape[0]
     mini_batches = list()
