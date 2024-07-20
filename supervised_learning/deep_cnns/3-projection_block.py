c32e8a741d3001250d45fec22119ddfa1d83cb5f
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-20 04:18:29 +0100

    3-projection_block.py

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python3
-""" projection of block as described """
+"""Projection of block as described"""
 from tensorflow import keras as K
 
 
 def projection_block(A_prev, filters, s=2):
-    """ projection of block as described """
+    """Projection block for resnet."""
     F11, F3, F12 = filters
-    """ convolution 1"""
+    init = K.initializers.he_normal(seed=0)  
+    """Using the same initializer with a seed for reproducibility"""
+
+    """Convolution 1"""
     c1 = K.layers.Conv2D(
         filters=F11, kernel_size=(
             1, 1), strides=(
-            s, s), padding='same', kernel_initializer=K.initializers.he_normal(
-                seed=0))(A_prev)
-    """batch normalisation 1"""
+            s, s), padding='same', kernel_initializer=init)(A_prev)
+    """Batch Normalization 1"""
     b1 = K.layers.BatchNormalization(axis=3)(c1)
-    """relu activation function 1"""
+    """ReLU Activation 1"""
     a1 = K.layers.Activation('relu')(b1)
-    """ convolution 2"""
+
+    """Convolution 2"""
     c2 = K.layers.Conv2D(
         filters=F3, kernel_size=(
             3, 3), strides=(
-            1, 1), padding='same', kernel_initializer=K.initializers.he_normal(
-                seed=0))(a1)
-    """batch normalisation 2"""
+            1, 1), padding='same', kernel_initializer=init)(a1)
+    """Batch Normalization 2"""
     b2 = K.layers.BatchNormalization(axis=3)(c2)
-    """relu activation function 2"""
+    """ReLU Activation 2"""
     a2 = K.layers.Activation('relu')(b2)
-    """ convolution 3"""
+
+    """Convolution 3"""
     c3 = K.layers.Conv2D(
         filters=F12, kernel_size=(
             1, 1), strides=(
-            1, 1), padding='same', kernel_initializer=K.initializers.he_normal(
-                seed=0))(a2)
-    """batch normalisation 3"""
+            1, 1), padding='same', kernel_initializer=init)(a2)
+    """Batch Normalization 3"""
     b3 = K.layers.BatchNormalization(axis=3)(c3)
-    """ convolution 4"""
+
+    """Convolution 4 for shortcut connection"""
     c4 = K.layers.Conv2D(
         filters=F12, kernel_size=(
             1, 1), strides=(
-            s, s), padding='same', kernel_initializer=K.initializers.he_normal(
-                seed=0))(A_prev)
-    """batch normalisation 4"""
+            s, s), padding='same', kernel_initializer=init)(A_prev)
+    """Batch Normalization 4 for shortcut connection"""
     b4 = K.layers.BatchNormalization(axis=3)(c4)
-    """addition of result after batching convolution 3 and convolution 4"""
+
+    """Adding the shortcut to the main path"""
     add = K.layers.Add()([b3, b4])
-    """relu activation function for the output"""
+    """Final ReLU Activation"""
     output = K.layers.Activation('relu')(add)
     return output
