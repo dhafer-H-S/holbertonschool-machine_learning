f1b25471a02819e61563852fce04a1d9c4a62161
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-20 04:42:59 +0100

    lastfiles check

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -2,7 +2,7 @@
 """ DenseNet-121 """
 
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
