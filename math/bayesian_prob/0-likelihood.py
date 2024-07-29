7d6d528435f15715bca6eeb0330e19ff8cb6f213
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-07-29 23:25:36 +0100

    calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -20,12 +20,10 @@ def likelihood(x, n, P):
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray):
-        raise ValueError("P must be a 1D numpy.ndarray")
-    if len(P.shape) != 1:
-        raise ValueError("P must be a 1D numpy.ndarray")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
     for p in P:
-        if np.any((p < 0) | (p > 1)):
+        if p < 0 or p > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
 
     res = 1
