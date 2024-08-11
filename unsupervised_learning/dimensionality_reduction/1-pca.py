a77403c95a03a658818bd0642a85b66e9d7a681b
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-11 17:30:21 +0100

    pca v2

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
+"""
+pca v2
+"""
 
 import numpy as np
 
 
 def pca(X, ndim):
     """fuction that performs PCA on a dataset"""
+    """normalisation for values"""
     X_centered = X - np.mean(X, axis=0)
+    
     cov_matrix = np.cov(X, rowvar=False)
     eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
     sorted_idx = np.argsort(eigenvalues)[::-1]
     sorted_eigenvalues = eigenvalues[sorted_idx]
     sorted_eigenvectors = eigenvectors[:, sorted_idx]
-
-
     W = sorted_eigenvectors[:, :ndim]
     T = np.dot(X_centered, W)
+
 
     return T
