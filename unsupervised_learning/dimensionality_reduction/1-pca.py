47f90cc2ad2417fc04372f3d622162cd8efff128
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-11 18:09:48 +0100

    pca v2

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -10,20 +10,7 @@ import numpy as np
 def pca(X, ndim):
     """Function that performs PCA on a dataset."""
     X_centered = X - np.mean(X, axis=0)
-    cov_matrix = np.cov(X_centered, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-    sorted_idx = np.argsort(eigenvalues)[::-1]
-    sorted_eigenvalues = eigenvalues[sorted_idx]
-    sorted_eigenvectors = eigenvectors[:, sorted_idx]
-
-    ndim = min(ndim, sorted_eigenvectors.shape[1])
-
-    W = sorted_eigenvectors[:, :ndim]
-
-    # Ensure the sign of the eigenvectors aligns with the expected output
-    for i in range(ndim):
-        if W[0, i] < 0:  # Flip sign if the first element is negative
-            W[:, i] = -W[:, i]
-
+    U, S, Vt = np.linalg.svd(X_centered, full_matrices=False)
+    W = Vt.T[:, :ndim]
     T = np.dot(X_centered, W)
     return T
