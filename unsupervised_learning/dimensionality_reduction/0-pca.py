c72ea51d39763724a7cdf768f7c16b21e916d62c
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-11 16:22:31 +0100

    pca

---
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """fuction that performs PCA on a dataset"""
+    """
+    value : The left singular vectors
+    vector : The singular values
+    (diagonal elements of the diagonal matrix from SVD)
+    sigma is the strength of each value or The right singular vectors
+    """
+    value, vector, sigma = np.linalg.svd(X)
+    """
+    cumsum : calculates the cumulative sum of the singular values
+    sum calculates the totla sum of all singular values
+    the devision gives the cumultative variance 
+    """
+    cumltative_var = np.cumsum(vector) / np.sum(vector)
+    r = next((i for i, v in enumerate(cumltative_var) if v >= var)) + 1
+    """
+    sigma takes the first r columns of the rigt singular vectors which
+    correspond to the top 'r' principal components
+    """
+    W = sigma[:r].T
+    return W
