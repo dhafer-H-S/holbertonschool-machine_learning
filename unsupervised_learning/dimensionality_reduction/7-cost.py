e33dd59792b3836eb1ab92c91e58ffa729f8b33e
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-13 00:09:33 +0100

    cost

--- unsupervised_learning/dimensionality_reduction/7-cost.py
+++ unsupervised_learning/dimensionality_reduction/7-cost.py
@@ -7,11 +7,11 @@ a function that calculate looss or cost in T-sne
 import numpy as np
 
 def cost(P, Q):
-    # Ensure no division by zero or log of zero
+    """Ensure no division by zero or log of zero"""
     P = np.maximum(P, 1e-12)
     Q = np.maximum(Q, 1e-12)
 
-    # Calculate the cost function (KL divergence)
+    """Calculate the cost function (KL divergence)"""
     C = np.sum(P * np.log(P / Q))
 
     return C
