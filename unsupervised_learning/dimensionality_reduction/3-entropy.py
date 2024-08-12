4df9ac4f5f1af6531e9d3b56638b8365be42bd7a
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-12 20:09:45 +0100

    entropy

---
+++ unsupervised_learning/dimensionality_reduction/3-entropy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pyhton3
+
+"""
+calculate the entropy in t-sne you're essentially quantifying
+how spread out or concentreated the similarities are between
+one point and all other points this helps to control how
+the algorithms represnts the local structure of the data in
+the lower dimensional space
+"""
+
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    compute the affinities 
+    """
+    Pi = np.exp(-Di * beta)
+    sum_Pi = np.sum(Pi)
+    Pi /= sum_Pi
+    """
+    compute shannon entropy
+    """
+    Hi = -np.sum(Pi * np.log2(Pi))
+    return Hi, Pi
