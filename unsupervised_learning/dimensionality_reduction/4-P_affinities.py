62436e64e80414313f9da9bd5ca142b26adabeb9
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-12 21:11:48 +0100

    p affinities

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -11,10 +11,19 @@ this function calculates the symmetric p affinities for the entire dataset
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """Calculate the p affinities for a given dataset.
+
+    Args:
+        X (numpy.ndarray): The input dataset of shape (n, d).
+        tol (float, optional): Tolerance value for convergence. Defaults to 1e-5.
+        perplexity (float, optional): Perplexity value for determining the number of nearest neighbors. Defaults to 30.0.
+
+    Returns:
+        numpy.ndarray: The p affinities matrix of shape (n, n).
+    """
     """p affinities"""
     n, d = X.shape
     D, P, betas, H = P_init(X, perplexity)
-
     for i in range(n):
         Di = D[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))]
         beta_min = None
@@ -36,6 +45,5 @@ def P_affinities(X, tol=1e-5, perplexity=30.0):
                     betas[i] = (betas[i] + beta_min) / 2.0
             Hi, Pi = HP(Di, betas[i])
         P[i, np.concatenate((np.r_[0:i], np.r_[i+1:n]))] = Pi
-
     P = (P + P.T) / (2 * n)
     return P
