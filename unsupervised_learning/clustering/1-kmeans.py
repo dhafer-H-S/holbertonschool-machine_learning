3d34fa4372440cbffabf25af80b3cb286dbce937
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-16 03:45:18 +0100

    The goal of K-means is to minimize the distance between data points and their cluster’s centroid

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -49,7 +49,7 @@ def kmeans(X, k, iterations=1000):
             int) or k <= 0:
         return None, None
 
-    # Initialize the centroids using the initialize function
+    """nitialize the centroids using the initialize function"""
     centroids = initialize(X, k)
     if centroids is None:
         return None, None
@@ -57,22 +57,20 @@ def kmeans(X, k, iterations=1000):
     n, d = X.shape
 
     for i in range(iterations):
-        # Step 4: Calculate the Euclidean distance between each data point and
-        # each centroid
+        """Step 4: Calculate the Euclidean distance between each data point and each centroid"""
         distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
 
-        # Step 5: Assign each data point to the closest centroid
+        """Step 5: Assign each data point to the closest centroid"""
         clss = np.argmin(distances, axis=1)
 
-        # Step 6: Calculate new centroids
+        """Step 6: Calculate new centroids"""
         new_centroids = np.array([
             X[clss == j].mean(axis=0) if np.any(clss == j)
             else np.random.uniform(np.min(X, axis=0), np.max(X, axis=0), size=d)
             for j in range(k)
         ])
 
-        # Step 7: Check for convergence (if centroids haven't changed, exit
-        # early)
+        """Step 7: Check for convergence (if centroids haven't changed, exit early)"""
         if np.all(centroids == new_centroids):
             break
 
