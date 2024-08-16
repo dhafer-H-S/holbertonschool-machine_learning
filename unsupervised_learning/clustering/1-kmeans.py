2b7bf2b0230d4173c2544cc0b1dbd4f9cc83a268
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-16 04:13:01 +0100

    The goal of K-means is to minimize the distance between data points and their cluster’s centroid

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,40 +1,32 @@
 #!/usr/bin/env python3
-
 """
-Function that performs k-means on a dataset.
+The k-means init values
+to start the algorithm
 """
-
 import numpy as np
 
 
 def initialize(X, k):
     """
-    Initializes cluster centroids using a uniform distribution.
-
-    Parameters:
-    - X: numpy.ndarray of shape (n, d), the dataset.
-    - k: a positive integer, number of clusters.
-
-    Returns:
-    - centroids: numpy.ndarray of shape (k, d), containing the initialized centroids."""
+    Using the multivariate uniform
+    distribution to have the intial values
+    """
     try:
-        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            raise ValueError("X must be a 2D numpy array.")
-        if not isinstance(k, int) or k <= 0:
-            raise ValueError("k must be a positive integer.")
-
-        n, d = X.shape
-        min_val = np.min(X, axis=0)
-        max_val = np.max(X, axis=0)
-        centroids = np.random.uniform(min_val, max_val, size=(k, X.shape[1]))
-        return centroids
+        if k <= 0 or not isinstance(k, int):
+            return None
+        x_min = np.min(X, axis=0)
+        x_max = np.max(X, axis=0)
+        init = np.random.uniform(x_min, x_max, size=(k, X.shape[1]))
+        return init
     except Exception as e:
-        print(f"Error during initialization: {e}")
         return None
 
 
 def kmeans(X, k, iterations=1000):
-    """Using the multivariate uniform distribution to have the intial values"""
+    """
+    Using the multivariate uniform
+    distribution to have the intial values
+    """
     try:
         C = initialize(X, k)
         for _ in range(iterations):
