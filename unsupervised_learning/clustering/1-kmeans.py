98d79f8ccff380be2b745b57bd89e249c39f6c2d
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-16 04:49:41 +0100

    The goal of K-means is to minimize the distance between data points and their cluster’s centroid

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
-""" 1. K-means """
+"""clustering"""
 import numpy as np
 
 
 def kmeans(X, k, iterations=1000):
-    """ performs K-means on a dataset """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    """performs K-means on a dataset"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
     if not isinstance(k, int) or k <= 0:
         return None, None
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
-
-    n, d = X.shape
-
-    low = np.min(X, axis=0)
-    high = np.max(X, axis=0)
-    C = np.random.uniform(low, high, (k, d))
-
-    clss = np.zeros(n, dtype=int)
+    C = np.random.uniform(np.amin(X, axis=0),
+                          np.amax(X, axis=0), (k, X.shape[1]))
+    if C is None:
+        return None, None
 
     for _ in range(iterations):
-        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
-        new_clss = np.argmin(distances, axis=1)
+        C_copy = np.copy(C)
 
-        if np.array_equal(clss, new_clss):
+        distances = np.linalg.norm(X[:, None] - C_copy, axis=-1)
+        clusters = np.argmin(distances, axis=-1)
+
+        for j in range(k):
+            # if no data points assigned to cluster j, leave it unchanged
+            if X[clusters == j].size == 0:
+                C[j] = np.random.uniform(np.amin(X, axis=0),
+                                         np.amax(X, axis=0), (1, X.shape[1]))
+            else:
+                # else, mean of all data points assigned to cluster j
+                C[j] = np.mean(X[clusters == j], axis=0)
+
+        if np.array_equal(C, C_copy):
             break
-        clss = new_clss
 
-        new_C = np.array([X[clss == j].mean(axis=0) if np.any(
-            clss == j) else np.random.uniform(low, high) for j in range(k)])
-
-        if np.allclose(C, new_C):
-            break
-        C = new_C
-
-    return C, clss
+    return C, clusters
