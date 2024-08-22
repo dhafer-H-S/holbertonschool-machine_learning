f8045cd44a16107d4ff79ea45f98d84d297968a4
dhafer hamza sfaxi <140395809+dhafer-H-S@users.noreply.github.com>
2024-08-22 17:53:36 +0100

    Update 7-maximization.py

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-function that calculates maximization step
-in the EM algorithm for a GMM
+Function that calculates the maximization step
+in the EM algorithm for a GMM.
 """
 
 import numpy as np
@@ -10,9 +10,20 @@ import numpy as np
 
 def maximization(X, g):
     """
-    X of shape (n, d) containing the data set
-    g of shape (k, n) containing the posterior
-    probabilities for each data point in each cluster 
+    Performs the maximization step in the Expectation-Maximization (EM) algorithm for a Gaussian Mixture Model (GMM).
+
+    Parameters:
+    - X: numpy.ndarray of shape (n, d) containing the data set
+         n is the number of data points
+         d is the number of dimensions for each data point
+    - g: numpy.ndarray of shape (k, n) containing the posterior
+         probabilities for each data point in each cluster
+         k is the number of clusters
+
+    Returns:
+    - pi: numpy.ndarray of shape (k,) containing the updated priors for each cluster
+    - m: numpy.ndarray of shape (k, d) containing the updated means for each cluster
+    - S: numpy.ndarray of shape (k, d, d) containing the updated covariance matrices for each cluster
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
@@ -21,14 +32,11 @@ def maximization(X, g):
     if X.shape[0] != g.shape[1]:
         return None, None, None
     n, d = X.shape
-    k, n_check = g.shape
-    if n != n_check:
-        return None, None, None
-
+    k, _ = g.shape
     pi = np.sum(g, axis=1) / n
     m = np.dot(g, X) / np.sum(g, axis=1)[:, np.newaxis]
     S = np.zeros((k, d, d))
     for i in range(k):
-        sub = X - m[i]  # (n, d)
-        S[i] = np.dot(g[i] * sub.T, sub) / np.sum(g[i])
+        X_centered = X - m[i]
+        S[i] = np.dot(g[i] * X_centered.T, X_centered) / np.sum(g[i])
     return pi, m, S
