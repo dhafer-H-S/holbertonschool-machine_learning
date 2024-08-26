8bde505c950e3ca9f79bd1fafb24e981869de1ff
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-26 01:09:18 +0100

    bayesian information criterion

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -18,7 +18,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     or not isinstance(kmin, int) or kmin <= 0
     or kmax is not None and (not isinstance(kmax, int) or kmax < kmin)
     or not isinstance(iterations, int) or iterations <= 0
-    or isinstance(kmax, int) and kmax <= kmin
+    or isinstance(kmax, int) and kmax >= kmin
     or not isinstance(iterations, int) or iterations <= 0
     or not isinstance(tol, float) or tol < 0
     or not isinstance(verbose, bool)
