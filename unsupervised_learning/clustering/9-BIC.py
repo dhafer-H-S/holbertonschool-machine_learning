4ec8124a1d2bdfcf8af9f898562571d0d4ca44c2
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-08-26 02:15:20 +0100

    bayesian information criterion

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -14,51 +14,39 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     using the bayesian information criterion
     """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None, None, None, None, None
-    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
-        return None, None, None, None, None
+        return None, None, None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None, None, None
+    if kmax is not None and (not isinstance(kmax, int) or kmax < kmin):
+        return None, None, None, None
     if not isinstance(iterations, int) or iterations <= 0:
-        return None, None, None, None, None
+        return None, None, None, None
     if not isinstance(tol, float) or tol < 0:
-        return None, None, None, None, None
+        return None, None, None, None
     if not isinstance(verbose, bool):
-        return None, None, None, None, None
+        return None, None, None, None
 
     n, d = X.shape
     if kmax is None:
-        """Undefined, set to maximum possible """
         kmax = n
-    if not isinstance(kmax, int) or kmax < 1 or kmax < kmin or kmax > n:
-        return None, None, None, None
 
-    b = []
-    likelihoods = []
+    l = np.zeros(kmax - kmin + 1)
+    b = np.zeros(kmax - kmin + 1)
+    best_k = None
+    best_result = None
+    best_bic = np.inf
 
-    """ With each cluster size from kmin to kmax""" 
     for k in range(kmin, kmax + 1):
-        """ Find the best fit with the GMM and current cluster size k"""
-        pi, m, S, g, li = expectation_maximization(
+        pi, m, S, g, lkhd = expectation_maximization(
             X, k, iterations, tol, verbose)
+        p = k * (d + d * (d + 1) / 2) + k - 1
+        bic = p * np.log(n) - 2 * lkhd
+        l[k - kmin] = lkhd
+        b[k - kmin] = bic
 
-        if pi is None or m is None or S is None or g is None:
-            return None, None, None, None
-        """ NOTE p is the number of parameters, so k * d with the means,"""
-        """k * d * (d + 1) with the covariance matrix, and k - 1 with the priors """
-        p = (k * d) + (k * d * (d + 1) // 2) + (k - 1)
-        bic = p * np.log(n) - 2 * li
-
-        """ Save log likelihood and BIC value with current cluster size"""
-        likelihoods.append(li)
-        b.append(bic)
-
-        """ Compare current BIC to best observed BIC"""
-        if k == kmin or bic < best_bic:
-            """ Update the return values"""
+        if bic < best_bic:
             best_bic = bic
-            best_results = (pi, m, S)
             best_k = k
+            best_result = (pi, m, S)
 
-    likelihoods = np.array(likelihoods)
-    l = likelihoods
-    b = np.array(b)
-    return best_k, best_results, l, b
+    return best_k, best_result, l, b
