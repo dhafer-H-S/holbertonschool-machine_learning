4d515aac7b0408d8b3ea1cf494210d09c607db7a
dhafer hamza sfaxi <140395809+dhafer-H-S@users.noreply.github.com>
2024-08-26 21:47:09 +0100

    Update 11-gmm.py

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -8,9 +8,9 @@ def gmm(X, k):
     """calculates the gmm using sklearn"""
     gmm = sk.GaussianMixture(n_components=k)
     gmm.fit(X)
-    pi = gmm.weight_
+    pi = gmm.weights_
     m = gmm.means_
-    S = gmm.covariance_
+    S = gmm.covariances_
     clss = gmm.predict(X)
     bic = gmm.bic(X)
     return pi ,m , S, clss, bic
