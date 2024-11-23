2f4338426f844557704fddf16cefcb5303560930
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-11-24 00:56:38 +0100

    Wasserstein GANs

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -34,7 +34,8 @@ class WGAN_clip(keras.Model):
         self.generator.loss = lambda x: - \
             tf.math.reduce_mean(self.discriminator(x))
         self.generator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
+            learning_rate=
+            self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
         self.generator.compile(
             optimizer=self.generator.optimizer,
             loss=self.generator.loss)
@@ -43,7 +44,8 @@ class WGAN_clip(keras.Model):
         self.discriminator.loss = lambda x, y: tf.math.reduce_mean(
             self.discriminator(y)) - tf.math.reduce_mean(self.discriminator(x))
         self.discriminator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
+            learning_rate=
+            self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
         self.discriminator.compile(
             optimizer=self.discriminator.optimizer,
             loss=self.discriminator.loss)
@@ -81,9 +83,10 @@ class WGAN_clip(keras.Model):
             gradients_of_discriminator = disc_tape.gradient(
                 discr_loss, self.discriminator.trainable_variables)
             self.discriminator.optimizer.apply_gradients(
-            zip(
-            gradients_of_discriminator, self.discriminator.trainable_variables
-            ))
+            zip
+            (gradients_of_discriminator
+            , self.discriminator.trainable_variables)
+            )
 
             """Clip the weights of the discriminator between -1 and 1"""
             for weight in self.discriminator.trainable_variables:
