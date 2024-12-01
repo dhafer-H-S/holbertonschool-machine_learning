931710795fd5047110bcc5c3096a2d58ad9f890d
dhafer hamza sfaxi <140395809+dhafer-H-S@users.noreply.github.com>
2024-12-01 02:53:31 +0100

    Update forecast_btc.py

--- supervised_learning/time_series/forecast_btc.py
+++ supervised_learning/time_series/forecast_btc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""
+file code for forcasting btc 
+"""
 from tensorflow import keras as k
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import datetime as dt
 import tensorflow as tf
 from preprocess_data import preprocessing
 
@@ -28,6 +29,7 @@ class WindowGenerator:
         """
 
         super().__init__()
+        
         # Store the raw data.
         self.train_df = train_df
         self.val_df = val_df
@@ -55,10 +57,6 @@ class WindowGenerator:
         self.labels_slice = slice(self.label_start, None)
         self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
 
-        # Create labels
-        self.train_labels = self.train_df[self.label_columns]
-        self.val_labels = self.val_df[self.label_columns]
-        self.test_labels = self.test_df[self.label_columns]
     def split_window(self, features):
         """
         Split the window into inputs and labels.
@@ -85,50 +83,6 @@ class WindowGenerator:
 
         return inputs, labels
 
-    def plot(self, model=None, plot_col='Close', max_subplots=3):
-        """
-        Plot the input and label data.
-
-        Args:
-            model (tf.keras.Model): Trained model for making predictions.
-            plot_col (str): Column name to plot.
-            max_subplots (int): Maximum number of subplots to display.
-        """
-
-        inputs, labels = self.example
-        plt.figure(figsize=(12, 8))
-        plot_col_idx = self.column_indices[plot_col]
-        max_n = min(max_subplots, len(inputs))
-        for n in range(max_n):
-            plt.subplot(3, 1, n + 1)
-            plt.ylabel('{} [normed]'.format(plot_col))
-            plt.plot(self.input_indices, inputs[n, :, plot_col_idx],
-                     label='Inputs', marker='.', zorder=-10)
-
-            if self.label_columns:
-                label_col_idx = self.label_columns_indices.get(plot_col,
-                                                               None)
-            else:
-                label_col_idx = plot_col_idx
-
-            if label_col_idx is None:
-                continue
-
-            plt.scatter(self.label_indices, labels[n, :, label_col_idx],
-                        edgecolors='k', label='Labels', c='#2ca02c', s=64)
-            if model is not None:
-                prediction = model(inputs)
-                plt.scatter(self.label_indices,
-                            prediction[n, :, label_col_idx],
-                            marker='X', edgecolors='k',
-                            label='Predictions',
-                            c='#ff7f0e', s=64)
-
-            if n == 0:
-                plt.legend()
-
-        plt.xlabel('Time [h]')
-
     def make_dataset(self, data):
         """
         Convert the data to a tf.dataset.
@@ -147,11 +101,12 @@ class WindowGenerator:
             sequence_length=self.total_window_size,
             sequence_stride=1,
             shuffle=True,
-            batch_size=32, )
+            batch_size=32)
 
         ds = ds.map(self.split_window)
 
         return ds
+
     @property
     def train_dataset(self):
         """
@@ -162,6 +117,7 @@ class WindowGenerator:
         """
 
         return self.make_dataset(self.train_df)
+
     @property
     def validation_dataset(self):
         """
@@ -172,6 +128,7 @@ class WindowGenerator:
         """
 
         return self.make_dataset(self.val_df)
+
     @property
     def test_dataset(self):
         """
@@ -182,59 +139,13 @@ class WindowGenerator:
         """
 
         return self.make_dataset(self.test_df)
-    @property
-    def val(self):
-        """
-        Get the validation dataset.
-
-        Returns:
-            ds (tf.data.Dataset): Validation dataset.
-        """
-
-        return self.validation_dataset
-
-    
-# class Baseline(tf.keras.Model):
-#     """
-#     Baseline class
-#     """
-
-#     def __init__(self, label_index=None):
-#         """
-#         Class constructor
-#         Argumetns:
-#          - label_index
-#         """
-
-#         super().__init__()
-#         self.label_index = label_index
-
-#     def call(self, inputs):
-#         """
-#         call
-#         Argumetns:
-#          - param inputs
-#         """
-#         if self.label_index is None:
-#             return inputs
-#         result = inputs[:, :, self.label_index]
-#         return result[:, :, tf.newaxis]
-
-
 
 def compile_and_fit(model, window, patience=2, epochs=500):
-    """compile and fit model"""
-    e_s = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
-                                           patience=patience,
-                                           mode='min')
+    """Compile and fit the model"""
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience, mode='min')
 
-    model.compile(loss=tf.losses.MeanSquaredError(),
-                  optimizer=tf.optimizers.Adam(),
-                  metrics=[tf.metrics.MeanAbsoluteError()])
-    history = model.fit(window.train_dataset, validation_data=(window.validation_dataset), epochs=epochs, callbacks=[e_s])
-
-    print(model.summary())
-
+    model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam(), metrics=[tf.metrics.MeanAbsoluteError()])
+    history = model.fit(window.train_dataset, validation_data=window.validation_dataset, epochs=epochs, callbacks=[early_stopping])
 
     return history
 
@@ -247,29 +158,23 @@ def forecasting(train, validation, test):
      - test is the test values
     """
 
-    window = WindowGenerator(input_width=24, label_width=24, shift=1,
+    window = WindowGenerator(input_width=24, label_width=1, shift=1,
                              train_df=train, val_df=validation, test_df=test,
                              label_columns=['Close'])
-    column_indices = window.column_indices
-    print(window)
-
-    val_performance = {}
-    performance = {}
-
-    # baseline = Baseline(label_index=column_indices['Close'])
-
-    # baseline.compile(loss=tf.keras.losses.MeanSquaredError(),
-    #                  metrics=[tf.keras.metrics.MeanAbsoluteError()])
 
     lstm_model = tf.keras.models.Sequential([
         tf.keras.layers.LSTM(32, return_sequences=True),
         tf.keras.layers.Dense(units=1)])
     history = compile_and_fit(lstm_model, window)
 
-    val_performance['LSTM'] = lstm_model.evaluate(window.val)
-    performance['LSTM'] = lstm_model.evaluate(window.test, verbose=0)
+    val_performance = lstm_model.evaluate(window.validation_dataset)
+    test_performance = lstm_model.evaluate(window.test_dataset, verbose=0)
     window.plot(lstm_model)
 
+    print(f"Validation performance: {val_performance}")
+    print(f"Test performance: {test_performance}")
+
 # Call the function with the paths to your CSV files
-x_train, x_valid, x_test = preprocessing(['bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', 'coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv'])
-forecasting(x_train, x_valid, x_test)
+df_train, df_valid, df_test = preprocessing(['bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', 'coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv'],
+                                         ['x_train.csv', 'x_valid.csv', 'x_test.csv'])
+forecasting(df_train, df_valid, df_test)
