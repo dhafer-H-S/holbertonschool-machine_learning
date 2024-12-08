549ce6767fa633e592057a2ac1cd352a0c4ccef1
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-08 02:53:50 +0100

    import only gensim

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -25,8 +25,8 @@ def fasttext_model(
         negative (int): The number of negative samples to use during training.
         window (int): The maximum distance between the current and predicted
         word within a sentence.
-        cbow (bool): Whether to use the Continuous Bag of Words (CBOW) architecture.
-        If False, use Skip-gram.
+        cbow (bool): Whether to use the Continuous Bag of Words (CBOW)
+        architecture If False, use Skip-gram.
         iterations (int): The number of iterations (epochs) over the corpus.
         seed (int): The seed for the random number generator.
         workers (int): The number of worker threads to train the model.
