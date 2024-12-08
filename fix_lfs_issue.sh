04683e6e142078541d7ef036537c44a2f3ca6284
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-08 01:29:53 +0100

    Remove all LFS files

--- fix_lfs_issue.sh
+++ fix_lfs_issue.sh
@@ -1,23 +1,39 @@
+# #!/bin/bash
+
+# # List all LFS-tracked files
+# lfs_files=$(git lfs ls-files -n)
+
+# # Untrack all LFS files
+# for file in $lfs_files; do
+#     git lfs untrack "$file"
+# done
+
+# # Remove cached files
+# for file in $lfs_files; do
+#     git rm --cached "$file"
+# done
+
+# # Add files back
+# git add .
+
+# # Commit the changes
+# git commit -m "Remove LFS tracking from all files"
+
+# # Force push the changes
+# git push --force origin main
 #!/bin/bash
 
-# List all LFS-tracked files
-lfs_files=$(git lfs ls-files -n)
-
 # Untrack all LFS files
-for file in $lfs_files; do
-    git lfs untrack "$file"
-done
+git lfs untrack "*"
 
-# Remove cached files
-for file in $lfs_files; do
-    git rm --cached "$file"
-done
+# Remove all LFS files from the repository
+git rm -r --cached .
 
-# Add files back
+# Add all files back to the repository
 git add .
 
 # Commit the changes
-git commit -m "Remove LFS tracking from all files"
+git commit -m "Remove all LFS files"
 
 # Force push the changes
 git push --force origin main
