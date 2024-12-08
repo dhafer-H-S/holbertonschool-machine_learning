db9bd83cf323eea2551c7f5e3b6d9aa2cf5aa86a
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-08 01:42:08 +0100

    Remove all LFS files

--- fix_lfs_issue.sh
+++ fix_lfs_issue.sh
@@ -1,30 +1,3 @@
-# #!/bin/bash
-
-# # List all LFS-tracked files
-# lfs_files=$(git lfs ls-files -n)
-
-# # Untrack all LFS files
-# for file in $lfs_files; do
-#     git lfs untrack "$file"
-# done
-
-# # Remove cached files
-# for file in $lfs_files; do
-#     git rm --cached "$file"
-# done
-
-# # Add files back
-# git add .
-
-# # Commit the changes
-# git commit -m "Remove LFS tracking from all files"
-
-# # Force push the changes
-# git push --force origin main
-#!/bin/bash
-
-#!/bin/bash
-
 #!/bin/bash
 
 # Untrack all LFS files
@@ -39,8 +12,8 @@ git add .
 # Commit the changes
 git commit -m "Remove all LFS files"
 
-# Remove LFS files and their history
-git filter-repo --path-glob '*.lfs' --invert-paths --force
+# Remove large files from the repository history
+git filter-repo --strip-blobs-bigger-than 100M --force
 
 # Re-add the remote
 git remote add origin https://github.com/dhafer-H-S/holbertonschool-machine_learning.git
