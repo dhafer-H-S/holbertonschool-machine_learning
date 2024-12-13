6adffd9395802bfd3f68c57a4554330af9566051
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-13 09:35:44 +0100

    rnn decoder

---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+RNN Decoder
+"""
+import tensorflow as tf
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """RNNDecoder class"""
+
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Class constructor
+        :param vocab: an integer representing the size of the output vocabulary
+        :param embedding: an integer representing the dimensionality of the embedding
+        vector
+        :param units: an integer representing the number of hidden units in the RNN cell
+        :param batch: an integer representing the batch size
+        """
+        super(RNNDecoder, self).__init__()
+        self.batch = batch
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(
+            units, return_sequences=True,
+            return_state=True, recurrent_initializer='glorot_uniform')
+        self.F = tf.keras.layers.Dense(vocab)
+        self.attention = __import__('1-self_attention').SelfAttention(units)
+
+    def call(self, x, s_prev, hidden_states):
+        """
+        :param x: a tensor of shape (batch, 1) containing the previous word in
+        the target sequence as an index of the target vocabulary
+        :param s_prev: a tensor of shape (batch, units) containing the previous
+        decoder hidden state
+        :param hidden_states: a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the encoder
+        :return: y, s
+        """
+        context_vector, attention_weights = self.attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
+        return y, s
