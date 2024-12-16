e6e09f19b98d3a2628a9276cffad37c6faad96d4
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-16 01:37:05 +0100

    multi head attention function

---
+++ supervised_learning/attention/6-multihead_attention.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Multi Head Attention
+"""
+
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """
+    Class to perform multi head attention
+    """
+
+    def __init__(self, dm, h):
+        """
+        dm - an integer representing the dimensionality of the model
+        h - an integer representing the number of heads
+        dm is divisible by h
+        """
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.dm = dm
+        self.depth = dm // h
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def split_heads(self, x, batch_size):
+        """
+        Split the last dimension
+        """
+        x = tf.reshape(x, (batch_size, -1, self.h, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, Q, K, V, mask):
+        """
+        Q - tensor of shape (batch, seq_len_q, dk)
+            containing the input to generate the query matrix
+        K - tensor of shape (batch, seq_len_v, dk)
+            containing the input to generate the key matrix
+        V - tensor of shape (batch, seq_len_v, dv)
+            containing the input to generate the value matrix
+        mask - always None
+        Returns: output, weights
+        output - tensor with its last two dimensions as
+            (..., seq_len_q, dm) containing the scaled dot product attention
+        weights - tensor with its last three dimensions as
+            (..., h, seq_len_q, seq_len_v) containing the attention weights
+        """
+        batch_size = tf.shape(Q)[0]
+        q = self.Wq(Q)
+        k = self.Wk(K)
+        v = self.Wv(V)
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        output, weights = sdp_attention(q, k, v, mask)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        output = tf.reshape(output, (batch_size, -1, self.dm))
+        return self.linear(output), weights
