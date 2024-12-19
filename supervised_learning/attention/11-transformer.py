2652f6fd2bbe4fcf57207cc2d26b5744d1c8de94
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-19 21:44:49 +0100

    transformer network

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -1,59 +1,37 @@
 #!/usr/bin/env python3
-"""transformer network"""
+"""
+transformer network
+"""
 import tensorflow as tf
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
 
 class Transformer(tf.keras.Model):
-    """Transformer class"""
+    """
+    class to create an transformer network
+    """
 
-    def __init__(
-            self,
-            N,
-            dm,
-            h,
-            hidden,
-            input_vocab,
-            target_vocab,
-            max_seq_input,
-            max_seq_target,
-            drop_rate=0.1):
-        """constructor"""
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 target_vocab, max_seq_input, max_seq_target, drop_rate=0.1):
+        """
+        Class constructor
+        """
         super(Transformer, self).__init__()
-        self.encoder = Encoder(
-            N,
-            dm,
-            h,
-            hidden,
-            input_vocab,
-            max_seq_input,
-            drop_rate)
-        self.decoder = Decoder(
-            N,
-            dm,
-            h,
-            hidden,
-            target_vocab,
-            max_seq_target,
-            drop_rate)
+        self.encoder = Encoder(N, dm, h, hidden,
+                               input_vocab, max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden,
+                               target_vocab, max_seq_target, drop_rate)
         self.linear = tf.keras.layers.Dense(target_vocab)
+        self.softmax = tf.keras.layers.Softmax(axis=-1)
 
-    def call(
-            self,
-            inputs,
-            target,
-            training,
-            encoder_mask,
-            look_ahead_mask,
-            decoder_mask):
-        """Returns: a tensor of shape (batch, target_seq_len, target_vocab)
-        containing the transformer output"""
-        encoder_out = self.encoder(inputs, training=training, mask=encoder_mask)
-        decoder_out = self.decoder(
-            target,
-            encoder_out,
-            training=training,
-            look_ahead_mask=look_ahead_mask,
-            mask=decoder_mask)
-        return self.linear(decoder_out)
+    def call(self, inputs, target, training,
+             encoder_mask, look_ahead_mask, decoder_mask):
+        """
+        call method for the transformer network
+        """
+        encoder_output = self.encoder(inputs, training, encoder_mask)
+        decoder_output = self.decoder(target, encoder_output, training,
+                                      look_ahead_mask, decoder_mask)
+        final_output = self.linear(decoder_output)
+        return final_output
