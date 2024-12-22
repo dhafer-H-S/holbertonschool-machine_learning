da2439e1a77a8d8c21cc4624387a90fb0aa8f5b5
dhafer-H-S <dhaferhamzasfaxi1@gmail.com>
2024-12-22 23:20:15 +0100

    task 0 creat function that preps and load data for machine learning translation

--- supervised_learning/transformer_apps/load_dataset.py
+++ supervised_learning/transformer_apps/load_dataset.py
@@ -4,5 +4,7 @@ import tensorflow_datasets as tfds
 
 pt2en_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
 for pt, en in pt2en_train.take(1):
-  print(pt.numpy().decode('utf-8'))
-  print(en.numpy().decode('utf-8'))
+    pt_text = pt.numpy().decode('utf-8').strip()
+    en_text = en.numpy().decode('utf-8').strip()
+    print(pt_text)
+    print(en_text)
